1954a4b97fd8d77516d4357c524837246495d143
Cleef <cleefgulfan312@gmail.com>
2025-06-19 19:07:53 +0100

    Added User entity.

---
+++ affiliate-proj/Core/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace affiliate_proj.Core.Entities;
+
+public class User
+{
+    public string uuid { get; }
+    public DateTime created_at { get; }
+    public string username { get; set; }
+    public string password { get; set; }
+}
