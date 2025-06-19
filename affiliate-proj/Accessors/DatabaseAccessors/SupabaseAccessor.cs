4a93faf7975e6e5cae94a11cfc0f72a88c55f26d
Cleef <cleefgulfan312@gmail.com>
2025-06-19 18:27:45 +0100

    Created SupabaseAccessor class.

---
+++ affiliate-proj/Accessors/DatabaseAccessors/SupabaseAccessor.cs
@@ -0,0 +1,13 @@
+﻿namespace affiliate_proj.Accessors.DatabaseAccessors;
+
+public class SupabaseAccessor
+{
+    private readonly string _url;
+    private readonly string _anonPubliKey;
+    private readonly string _serviceRoleKey;
+
+    public SupabaseAccessor()
+    {
+        
+    }
+}
