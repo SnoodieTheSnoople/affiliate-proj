733c7b4158c85e882fb149a1d6b38f6b8e0543f3
Cleef <cleefgulfan312@gmail.com>
2025-06-20 01:00:14 +0100

    Added Creator class attributes.

--- affiliate-proj/Core/Entities/Creator.cs
+++ affiliate-proj/Core/Entities/Creator.cs
@@ -2,5 +2,11 @@
 
 public class Creator
 {
-    
+    private string creator_id { get; set; }
+    private DateTime created_at { get; set; }
+    private string firstname  { get; set; }
+    private string lastname { get; set; }
+    private DateOnly dob { get; set; }
+    private string stripe_id  { get; set; }
+    private string user_id  { get; set; }
 }
