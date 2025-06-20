302fcfc0658df6e59a23e9a2bc86df069c7a1679
Cleef <cleefgulfan312@gmail.com>
2025-06-20 01:45:12 +0100

    Refactor - Changed from Uid to Id for consistency.

--- affiliate-proj/Core/Entities/CommissionRate.cs
+++ affiliate-proj/Core/Entities/CommissionRate.cs
@@ -3,7 +3,7 @@ namespace affiliate_proj.Core.Entities;
 public class CommissionRate
 {
     public string RateId {  get; set; }
-    public string CreatorUid { get; set; }
-    public string StoreUid { get; set; }
+    public string CreatorId { get; set; }
+    public string StoreId { get; set; }
     public float Rate { get; set; }
 }
