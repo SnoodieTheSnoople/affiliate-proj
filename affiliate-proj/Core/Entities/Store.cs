80df052c955a7c3ecf6e8c2ecb44b8d354bb0c4e
Cleef <cleefgulfan312@gmail.com>
2025-06-20 01:04:38 +0100

    Added Store entity and attributes.

---
+++ affiliate-proj/Core/Entities/Store.cs
@@ -0,0 +1,11 @@
+namespace affiliate_proj.Core.Entities;
+
+public class Store
+{
+    private string _storeId { get; set; }
+    private DateTime _createdAt { get; set; }
+    private string _storeName { get; set; }
+    private string _shopifyId { get; set; }
+    private string _shopifyToken { get; set; }
+    private string _storeUrl { get; set;  }
+}
