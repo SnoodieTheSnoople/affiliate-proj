8b8057336d0c9ba00035add6d3e11e3813c91f03
Cleef <cleefgulfan312@gmail.com>
2025-06-20 01:34:22 +0100

    Refactor - Changed access modifiers to public.

--- affiliate-proj/Core/Entities/Store.cs
+++ affiliate-proj/Core/Entities/Store.cs
@@ -2,10 +2,10 @@ namespace affiliate_proj.Core.Entities;
 
 public class Store
 {
-    private string _storeId { get; set; }
-    private DateTime _createdAt { get; set; }
-    private string _storeName { get; set; }
-    private string _shopifyId { get; set; }
-    private string _shopifyToken { get; set; }
-    private string _storeUrl { get; set;  }
+    public string StoreId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string StoreName { get; set; }
+    public string ShopifyId { get; set; }
+    public string ShopifyToken { get; set; }
+    public string StoreUrl { get; set;  }
 }
