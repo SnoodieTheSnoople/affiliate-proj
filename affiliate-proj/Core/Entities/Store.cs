f065735a16528874ac43a00861a278851a5b292c
Cleef <cleefgulfan312@gmail.com>
2025-06-21 00:41:10 +0100

    Added TODOs for Store future entity changes.

--- affiliate-proj/Core/Entities/Store.cs
+++ affiliate-proj/Core/Entities/Store.cs
@@ -7,5 +7,8 @@ public class Store
     public string StoreName { get; set; }
     public string ShopifyId { get; set; }
     public string ShopifyToken { get; set; }
-    public string StoreUrl { get; set;  }
+    public string StoreUrl { get; set; }
+
+    // TODO: Review entity on ER diagram.
+    // TODO: Ensure complies with user flow for authentication utilising User entity.
 }
