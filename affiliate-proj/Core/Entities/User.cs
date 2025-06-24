f6e153d661ee8bbf3f2a7fec83f95a3a941d5387
Cleef <cleefgulfan312@gmail.com>
2025-06-24 18:46:00 +0100

    Refactor - Attribute added to reflect table.

--- affiliate-proj/Core/Entities/User.cs
+++ affiliate-proj/Core/Entities/User.cs
@@ -5,5 +5,6 @@ public class User
     public string Uuid { get; set;  }
     public DateTime CreatedAt { get; set; }
     public string Username { get; set; }
+    public string Email { get; set; }
     public string Password { get; set; }
 }
