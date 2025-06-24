68075eb1dd3b99b43db578074ca861302dc60210
Cleef <cleefgulfan312@gmail.com>
2025-06-24 21:00:17 +0100

    Added data annotations to entity.

--- affiliate-proj/Core/Entities/User.cs
+++ affiliate-proj/Core/Entities/User.cs
@@ -1,10 +1,19 @@
-﻿namespace affiliate_proj.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace affiliate_proj.Core.Entities;
 
 public class User
 {
+    [Required]
     public string Uuid { get; set;  }
     public DateTime CreatedAt { get; set; }
+    
+    [Required]
     public string Username { get; set; }
+    
+    [Required]
     public string Email { get; set; }
+    
+    [Required]
     public string Password { get; set; }
 }
