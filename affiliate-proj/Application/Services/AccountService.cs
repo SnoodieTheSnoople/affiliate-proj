287728bfe5821dbd51269b97778663a75a731343
Cleef <cleefgulfan312@gmail.com>
2025-06-24 17:51:46 +0100

    Added constructor to pass singleton data accessor.

--- affiliate-proj/Application/Services/AccountService.cs
+++ affiliate-proj/Application/Services/AccountService.cs
@@ -1,6 +1,13 @@
-﻿namespace affiliate_proj.Application.Services;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+
+namespace affiliate_proj.Application.Services;
 
 public class AccountService
 {
-    
+    private readonly SupabaseAccessor _supabaseAccessor;
+
+    public AccountService(SupabaseAccessor supabaseAccessor)
+    {
+        _supabaseAccessor = supabaseAccessor;
+    }
 }
