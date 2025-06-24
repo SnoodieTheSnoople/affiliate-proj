bd18f81db296768181a428c3c1b31889771c0fc3
Cleef <cleefgulfan312@gmail.com>
2025-06-24 22:12:45 +0100

    Added DbContext to interact with PostgreSQL database directly.

--- affiliate-proj/Accessors/DatabaseAccessors/SupabaseAccessor.cs
+++ affiliate-proj/Accessors/DatabaseAccessors/SupabaseAccessor.cs
@@ -1,17 +1,34 @@
-﻿namespace affiliate_proj.Accessors.DatabaseAccessors;
+﻿using affiliate_proj.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class SupabaseAccessor
+namespace affiliate_proj.Accessors.DatabaseAccessors;
+
+public class SupabaseAccessor(DbContextOptions<SupabaseAccessor> options) : DbContext (options)
 {
     private readonly string _url;
     private readonly string _anonPublicKey;
     private readonly string _serviceRoleKey;
+    
+    public DbSet<User> Users { get; set; }
 
-    public SupabaseAccessor(string url,  string anonPublicKey, string serviceRoleKey)
+    // public SupabaseAccessor(string url,  string anonPublicKey, string serviceRoleKey)
+    // {
+    //     _url = url;
+    //     _anonPublicKey = anonPublicKey;
+    //     _serviceRoleKey = serviceRoleKey;
+    // }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        _url = url;
-        _anonPublicKey = anonPublicKey;
-        _serviceRoleKey = serviceRoleKey;
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().ToTable("users");
+        modelBuilder.Entity<User>().Property(x => x.UserId).HasColumnName("user_id");
+        modelBuilder.Entity<User>().Property(x => x.CreatedAt).HasColumnName("created_at");
+        modelBuilder.Entity<User>().Property(x => x.Username).HasColumnName("username");
+        modelBuilder.Entity<User>().Property(x => x.Email).HasColumnName("email");
+        modelBuilder.Entity<User>().Property(x => x.Password).HasColumnName("password");
     }
+    
     
     public void ShowKeys()
     {
