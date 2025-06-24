883f16dc4c47916a7e5fdd38de333d9b5b677df6
Cleef <cleefgulfan312@gmail.com>
2025-06-24 22:21:10 +0100

    Added UserDTO for data mapping.

---
+++ affiliate-proj/Core/DTOs/Account/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace affiliate_proj.Core.DTOs.Account;
+
+public class UserDTO
+{
+    public System.Guid Uuid { get; set; }
+    public string CreatedAt { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
