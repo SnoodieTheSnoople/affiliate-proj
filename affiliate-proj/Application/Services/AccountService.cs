7f1550872d0d7b357392a7bfa9bfa60b3851b3de
Cleef <cleefgulfan312@gmail.com>
2025-06-24 22:19:53 +0100

    Created AccountService to be concrete class of interface and added basic call to get all users from database.

--- affiliate-proj/Application/Services/AccountService.cs
+++ affiliate-proj/Application/Services/AccountService.cs
@@ -1,13 +1,22 @@
 ﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Application.Interfaces;
+using affiliate_proj.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace affiliate_proj.Application.Services;
 
-public class AccountService
+public class AccountService : IAccountService
 {
     private readonly SupabaseAccessor _supabaseAccessor;
 
     public AccountService(SupabaseAccessor supabaseAccessor)
     {
         _supabaseAccessor = supabaseAccessor;
+    }
+
+    public async Task<IEnumerable<User>> GetAllUsersAsync()
+    {
+        var users = _supabaseAccessor.Users.AsNoTracking().ToList();
+        return users;
     }
 }
