5bdb1fdaee3f1d5979f48761ff50b51bea867f0a
Cleef <cleefgulfan312@gmail.com>
2025-06-27 01:22:27 +0100

    Added placeholder EmailService for testing.

---
+++ affiliate-proj/Core/Placeholders/EmailService/NoOpEmailSender.cs
@@ -0,0 +1,16 @@
+﻿using affiliate_proj.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace affiliate_proj;
+
+public class NoOpEmailSender : IEmailSender<User>
+{
+    public Task SendConfirmationLinkAsync(User user, string email, string confirmationLink) 
+        => Task.CompletedTask;
+
+    public Task SendPasswordResetLinkAsync(User user, string email, string resetLink) 
+        => Task.CompletedTask;
+
+    public Task SendPasswordResetCodeAsync(User user, string email, string resetCode) 
+        => Task.CompletedTask;
+}
