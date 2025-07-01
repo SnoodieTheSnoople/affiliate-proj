e3d9cab9f229a7efee7fb31e31da3a6e7ad380d8
Cleef <cleefgulfan312@gmail.com>
2025-07-01 20:21:05 +0100

    Added more interface methods covering User and Creator.

--- affiliate-proj/Application/Interfaces/IAccountService.cs
+++ affiliate-proj/Application/Interfaces/IAccountService.cs
@@ -7,4 +7,15 @@ public interface IAccountService
     Task<IEnumerable<User>> GetAllUsersAsync();
     Task<User> GetUserByIdAsync(System.Guid userId);
     Task<User> GetUserByEmailAsync(string email);
+    
+    Task<User> GetUserNameAsync(System.Guid userId);
+    Task<User> GetPhoneNumberAsync(System.Guid userId);
+    
+    Task<User> SetUserNameAsync(User user);
+    Task<User> SetPhoneNumberAsync(User user);
+    
+    Task<User> SetFirstNameAsync(string firstname);
+    Task<User> SetLastNameAsync(string lastname);
+    Task<User> SetDateOfBirthAsync(DateTime dateofbirth);
+    
 }
