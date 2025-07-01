3fbf910b2e523516ae7d337f9c7d4d2e8fc0fb96
Cleef <cleefgulfan312@gmail.com>
2025-07-01 19:56:54 +0100

    Refactor - Changed to reflect Users table.

--- affiliate-proj/Core/Entities/User.cs
+++ affiliate-proj/Core/Entities/User.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Identity;
 
 namespace affiliate_proj.Core.Entities;
 
@@ -15,9 +14,8 @@ public class User
     [Required]
     public string Username { get; set; }
     
-    [Required]
-    public string Email { get; set; }
+    public DateTime DeletedAt { get; set; }
     
     [Required]
-    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
 }
