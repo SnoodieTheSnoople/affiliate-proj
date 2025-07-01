6bc3fc0f1ff79906c4b2e120c7abc6c2e970b037
Cleef <cleefgulfan312@gmail.com>
2025-07-01 15:24:12 +0100

    Removed unused lines.

--- affiliate-proj/Program.cs
+++ affiliate-proj/Program.cs
@@ -28,39 +28,18 @@ public class Program
         // Console.WriteLine(builder.Configuration.GetValue<string>("Supabase:ServiceRoleKey"));
         // Console.WriteLine(builder.Configuration.GetValue<string>("Postgres:ConnectionString"));
         
-        // builder.Services.AddSingleton<SupabaseAccessor>(tmp => new SupabaseAccessor(
-        //     builder.Configuration.GetValue<string>("Supabase:Url"),
-        //     builder.Configuration.GetValue<string>("Supabase:AnonPublicKey"),
-        //     builder.Configuration.GetValue<string>("Supabase:ServiceRoleKey"))
-        // );
+        builder.Services.AddSingleton<SupabaseAccessor>(tmp => new SupabaseAccessor(
+            builder.Configuration.GetValue<string>("Supabase:Url"),
+            builder.Configuration.GetValue<string>("Supabase:AnonPublicKey"),
+            builder.Configuration.GetValue<string>("Supabase:ServiceRoleKey"))
+        );
 
-        builder.Services.AddDbContext<SupabaseAccessor>((sp, options) =>
+        builder.Services.AddDbContext<PostgresDbContext>((sp, options) =>
         {
-            var config =  sp.GetRequiredService<IConfiguration>();
+            var config = sp.GetRequiredService<IConfiguration>();
             var connectionString = config.GetValue<string>("Postgres:ConnectionString");
-            
             options.UseNpgsql(connectionString);
         });
-
-        builder.Services.AddScoped<IAccountService, AccountService>();
-
-        builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
-            {
-                options.SignIn.RequireConfirmedPhoneNumber = false;
-                options.SignIn.RequireConfirmedAccount = false;
-                options.SignIn.RequireConfirmedAccount = false;
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<SupabaseAccessor>()
-            .AddDefaultTokenProviders();
-
-        builder.Services.AddSingleton<IEmailSender<User>, NoOpEmailSender>();
-
-        builder.Services.AddAuthorization();
 
         var app = builder.Build();
 
