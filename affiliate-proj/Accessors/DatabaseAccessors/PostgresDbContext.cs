8e80fb74d92911afa5e950af08f3183c38794abf
Cleef <cleefgulfan312@gmail.com>
2025-07-01 15:12:25 +0100

    Added constructor for DbContext initialisation.

--- affiliate-proj/Accessors/DatabaseAccessors/PostgresDbContext.cs
+++ affiliate-proj/Accessors/DatabaseAccessors/PostgresDbContext.cs
@@ -4,5 +4,9 @@ namespace affiliate_proj.Accessors.DatabaseAccessors;
 
 public class PostgresDbContext : DbContext
 {
+    public PostgresDbContext(DbContextOptions<PostgresDbContext> options)
+    {
+        
+    }
     
 }
