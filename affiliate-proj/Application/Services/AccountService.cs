1abc93d432ce2ba03e9f84366806f47b2f91d47f
Cleef <cleefgulfan312@gmail.com>
2025-07-01 20:24:34 +0100

    Added NotImplemented methods and DbContext.

--- affiliate-proj/Application/Services/AccountService.cs
+++ affiliate-proj/Application/Services/AccountService.cs
@@ -8,16 +8,11 @@ namespace affiliate_proj.Application.Services;
 public class AccountService : IAccountService
 {
     private readonly SupabaseAccessor _supabaseAccessor;
+    private readonly PostgresDbContext _postgresDbContext;
 
     public AccountService(SupabaseAccessor supabaseAccessor)
     {
         _supabaseAccessor = supabaseAccessor;
-    }
-
-
-    public Task<IEnumerable<User>> GetAllUsersAsync()
-    {
-        throw new NotImplementedException();
     }
 
     public Task<User> GetUserByIdAsync(Guid userId)
@@ -26,6 +21,41 @@ public class AccountService : IAccountService
     }
 
     public Task<User> GetUserByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserNameAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetPhoneNumberAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> SetUserNameAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> SetPhoneNumberAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> SetFirstNameAsync(string firstname)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> SetLastNameAsync(string lastname)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> SetDateOfBirthAsync(DateTime dateofbirth)
     {
         throw new NotImplementedException();
     }
