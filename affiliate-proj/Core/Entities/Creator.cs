0b29afeaa005eac1f2cb3a02fb552e59787bcd28
Cleef <cleefgulfan312@gmail.com>
2025-07-01 20:02:41 +0100

    Added model annotations.

--- affiliate-proj/Core/Entities/Creator.cs
+++ affiliate-proj/Core/Entities/Creator.cs
@@ -1,12 +1,27 @@
-﻿namespace affiliate_proj.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace affiliate_proj.Core.Entities;
 
 public class Creator
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string CreatorId { get; set; }
+    
     public DateTime CreatedAt { get; set; }
-    public string _Firstname  { get; set; }
+    
+    [Required]
+    public string Firstname  { get; set; }
+    
+    [Required]
     public string Lastname { get; set; }
+    
+    [Required]
     public DateOnly Dob { get; set; }
+    
     public string StripeId  { get; set; }
+    
+    [ForeignKey("UserId")]
     public string UserId  { get; set; }
 }
