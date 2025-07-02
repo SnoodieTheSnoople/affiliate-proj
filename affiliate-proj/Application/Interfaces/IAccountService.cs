f99248243ddd7940f859d53a6804027532be2ea7
Cleef <cleefgulfan312@gmail.com>
2025-07-02 15:25:21 +0100

    Changed return type.

--- affiliate-proj/Application/Interfaces/IAccountService.cs
+++ affiliate-proj/Application/Interfaces/IAccountService.cs
@@ -4,7 +4,7 @@ namespace affiliate_proj.Application.Interfaces;
 
 public interface IAccountService
 { 
-    string? GetUserByIdAsync(Guid userId);
+    Task<User?> GetUserByIdAsync(Guid userId);
     Task<User> GetUserByEmailAsync(string email);
     
     Task<User> GetUserNameAsync(System.Guid userId);
