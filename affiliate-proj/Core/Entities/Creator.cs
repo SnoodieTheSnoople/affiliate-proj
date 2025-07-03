5e672a586c8f9da9053a6b7f2bddadea99479dd8
Cleef <cleefgulfan312@gmail.com>
2025-07-03 18:26:26 +0100

    Refactor - Renamed to match source at database.

--- affiliate-proj/Core/Entities/Creator.cs
+++ affiliate-proj/Core/Entities/Creator.cs
@@ -16,7 +16,7 @@ public class Creator
     public string Firstname  { get; set; }
     
     [Required]
-    public string Lastname { get; set; }
+    public string Surname { get; set; }
     
     [Required]
     public DateTime Dob { get; set; }
