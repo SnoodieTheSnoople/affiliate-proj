3454aa6fe9aae49cae92e9662c723a6eff794a69
Cleef <cleefgulfan312@gmail.com>
2025-07-03 20:26:20 +0100

    Removed unnecessary getters and added more setters related to User.

--- affiliate-proj/Application/Interfaces/IAccountService.cs
+++ affiliate-proj/Application/Interfaces/IAccountService.cs
@@ -8,15 +8,13 @@ public interface IAccountService
     Task<Core.DTOs.Account.UserDTO?> GetUserByIdAsync(Guid userId);
     Task<UserDTO?> GetUserByEmailAsync(string email);
     
-    Task<UserDTO> GetUserNameAsync(System.Guid userId);
-    Task<UserDTO> GetPhoneNumberAsync(System.Guid userId);
-    
+    Task<UserDTO?> SetEmailAsync(string email);
     Task<UserDTO> SetUserNameAsync(User userDto);
     Task<UserDTO> SetPhoneNumberAsync(User userDto);
     
-    Task<UserDTO> SetFirstNameAsync(string firstname);
-    Task<UserDTO> SetLastNameAsync(string lastname);
-    Task<UserDTO> SetDateOfBirthAsync(DateTime dateofbirth);
+    Task<CreatorDTO> SetFirstNameAsync(string firstname);
+    Task<CreatorDTO> SetLastNameAsync(string lastname);
+    Task<CreatorDTO> SetDateOfBirthAsync(DateTime dateofbirth);
     Task<CreatorDTO?> GetCreatorByUserIdAsync(Guid userId);
     Task<CreatorDTO?> SetCreatorAsync(CreatorDTO creator);
 }
