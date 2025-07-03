3536784992ad675e3aa72e1eb45b93e10e528088
Cleef <cleefgulfan312@gmail.com>
2025-07-03 17:59:19 +0100

    Created CreatorDTO to reflect Creator entity.

---
+++ affiliate-proj/Core/DTOs/Account/CreatorDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace affiliate_proj.Core.DTOs.Account;
+
+public class CreatorDTO
+{
+    public string CreatorId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public string Firstname  { get; set; }
+    
+    public string Lastname { get; set; }
+    
+    public DateTime Dob { get; set; }
+    
+    public string? StripeId  { get; set; }
+    
+    public Guid UserId { get; set; }
+}
