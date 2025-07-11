329e892db899f1404c925abdc5d16f4d68e727c5
Cleef <cleefgulfan312@gmail.com>
2025-07-11 14:49:21 +0100

    Added CreatorRequest model for extracting data from request body.

---
+++ affiliate-proj/Core/RequestModels/CreatorRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace affiliate_proj.Core.RequestModels;
+
+public class CreatorRequest
+{
+    public Guid CreatorId { get; set; }
+    
+    public string? Firstname  { get; set; }
+    
+    public string? Surname { get; set; }
+    
+    public DateTime? Dob { get; set; }
+    
+    public string? StripeId  { get; set; }
+    
+    public Guid UserId { get; set; }
+}
