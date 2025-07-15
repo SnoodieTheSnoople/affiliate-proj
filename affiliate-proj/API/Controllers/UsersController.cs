15078f0260806e45948dab737be95a319cde3aff
Cleef <cleefgulfan312@gmail.com>
2025-07-15 22:37:01 +0100

    Refactor - Changed UpdateUserEmail to call upon UserService.cs

--- affiliate-proj/API/Controllers/UsersController.cs
+++ affiliate-proj/API/Controllers/UsersController.cs
@@ -85,7 +85,7 @@ namespace affiliate_proj.API.Controllers
 
             try
             {
-                var user = await _accountService.UpdateEmailAsync(request.Email, request.UserId);
+                var user = await _userService.UpdateEmailAsync(request.Email, request.UserId);
                 return Ok(user);
             }
             catch (UnauthorizedAccessException ex)
