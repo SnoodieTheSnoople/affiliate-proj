1d5905092a37bcabcd6fa7dda6587634dc54e0c6
Cleef <cleefgulfan312@gmail.com>
2025-07-15 13:52:52 +0100

    Added methods to respective concrete classes with no implementation.

--- affiliate-proj/Application/Services/Account/Creator/CreatorService.cs
+++ affiliate-proj/Application/Services/Account/Creator/CreatorService.cs
@@ -1,6 +1,37 @@
-﻿namespace affiliate_proj.Application.Services.Creator;
+﻿using affiliate_proj.Application.Interfaces.Creator;
+using affiliate_proj.Core.DTOs.Account;
 
-public class CreatorService
+namespace affiliate_proj.Application.Services.Creator;
+
+public class CreatorService : ICreatorService
 {
-    
+    public Task<CreatorDTO?> SetCreatorAsync(CreatorDTO creator, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreatorDTO?> GetCreatorByUserIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreatorDTO?> DeleteCreatorAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreatorDTO?> UpdateFirstNameAsync(string firstName, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreatorDTO?> UpdateSurnameAsync(string surname, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreatorDTO?> UpdateDateOfBirthAsync(DateTime dateofbirth, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- affiliate-proj/Application/Services/Account/User/UserService.cs
+++ affiliate-proj/Application/Services/Account/User/UserService.cs
@@ -1,6 +1,37 @@
-﻿namespace affiliate_proj.Application.Services.User;
+﻿using affiliate_proj.Application.Interfaces.User;
+using affiliate_proj.Core.DTOs.Account;
 
-public class UserService
+namespace affiliate_proj.Application.Services.User;
+
+public class UserService : IUserService
 {
-    
+    public Task<UserDTO?> SetUserAsync(UserDTO user, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDTO?> GetUserByUserIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDTO?> DeleteUser(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDTO?> UpdateEmailAsync(string email, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDTO?> UpdateUserNameAsync(string username, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDTO?> UpdatePhoneNumberAsync(string phoneNumber, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
 }
