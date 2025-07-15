c5c1a17b4a861f99359658a20444ec2f90620265
Cleef <cleefgulfan312@gmail.com>
2025-07-15 13:48:47 +0100

    Added User related interface methods.

--- affiliate-proj/Application/Interfaces/Account/User/IUserService.cs
+++ affiliate-proj/Application/Interfaces/Account/User/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace affiliate_proj.Application.Interfaces.User;
+﻿using affiliate_proj.Core.DTOs.Account;
+
+namespace affiliate_proj.Application.Interfaces.User;
 
 public interface IUserService
 {
-    
+    Task<UserDTO?> SetUserAsync(UserDTO user, Guid userId);
+    Task<UserDTO?> GetUserByUserIdAsync(Guid userId);
+    Task<UserDTO?> DeleteUser(Guid userId);
+    Task<UserDTO?> UpdateEmailAsync(string email, Guid userId);
+    Task<UserDTO?> UpdateUserNameAsync(string username, Guid userId);
+    Task<UserDTO?> UpdatePhoneNumberAsync(string phoneNumber, Guid userId);
 }
