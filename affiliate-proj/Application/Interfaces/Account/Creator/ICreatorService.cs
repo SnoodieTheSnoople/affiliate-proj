10359a73f7c1a1861526c55ebeef325a49f42cbc
Cleef <cleefgulfan312@gmail.com>
2025-07-15 13:35:48 +0100

    Added Creator related interface methods.

--- affiliate-proj/Application/Interfaces/Account/Creator/ICreatorService.cs
+++ affiliate-proj/Application/Interfaces/Account/Creator/ICreatorService.cs
@@ -1,6 +1,13 @@
-﻿namespace affiliate_proj.Application.Interfaces.Creator;
+﻿using affiliate_proj.Core.DTOs.Account;
+
+namespace affiliate_proj.Application.Interfaces.Creator;
 
 public interface ICreatorService
 {
-    
+    Task<CreatorDTO?> SetCreatorAsync(CreatorDTO creator, Guid userId);
+    Task<CreatorDTO?> GetCreatorByUserIdAsync(Guid userId);
+    Task<CreatorDTO?> DeleteCreatorAsync (Guid userId);
+    Task<CreatorDTO?> UpdateFirstNameAsync(string firstName, Guid userId);
+    Task<CreatorDTO?> UpdateSurnameAsync(string surname, Guid userId);
+    Task<CreatorDTO?> UpdateDateOfBirthAsync(DateTime dateofbirth, Guid userId);
 }
