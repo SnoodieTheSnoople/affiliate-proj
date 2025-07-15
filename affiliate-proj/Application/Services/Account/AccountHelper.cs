f0a27ca037b89a62c5d45499684e71213622ff4e
Cleef <cleefgulfan312@gmail.com>
2025-07-15 16:30:17 +0100

    Added necessary properties and constructor.

--- affiliate-proj/Application/Services/Account/AccountHelper.cs
+++ affiliate-proj/Application/Services/Account/AccountHelper.cs
@@ -1,9 +1,19 @@
-﻿using affiliate_proj.Application.Interfaces;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Application.Interfaces;
 
 namespace affiliate_proj.Application.Services;
 
 public class AccountHelper : IAccountHelper
 {
+    private readonly PostgresDbContext _postgresDbContext;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AccountHelper(PostgresDbContext postgresDbContext, IHttpContextAccessor httpContextAccessor)
+    {
+        _postgresDbContext = postgresDbContext;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     public string GetUserIdFromAccessToken()
     {
         throw new NotImplementedException();
