ef0563dd9e8a7fe1bb9dbc43a0487ebc6406cfb9
Cleef <cleefgulfan312@gmail.com>
2025-07-16 22:34:45 +0100

    Removed migrated methods from AccountService.

--- affiliate-proj/Application/Interfaces/Account/IAccountService.cs
+++ affiliate-proj/Application/Interfaces/Account/IAccountService.cs
@@ -6,11 +6,4 @@ namespace affiliate_proj.Application.Interfaces;
 public interface IAccountService
 { 
     Task<UserDTO?> DeleteUser(Guid userId);
-    
-    Task<CreatorDTO?> UpdateFirstNameAsync(string firstname, Guid userId);
-    Task<CreatorDTO?> UpdateSurnameAsync(string surname, Guid userId);
-    Task<CreatorDTO?> UpdateDateOfBirthAsync(DateTime dateofbirth, Guid userId);
-    Task<CreatorDTO?> GetCreatorByUserIdAsync(Guid userId);
-    Task<CreatorDTO?> SetCreatorAsync(CreatorDTO creatorDto, Guid userId);
-    Task<CreatorDTO?> DeleteCreator(Guid userId, Guid piiReplacement);
 }
