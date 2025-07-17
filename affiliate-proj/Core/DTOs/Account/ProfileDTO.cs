aa00def2bf1a16f6f2da8cfbb6eb8947e9d240ab
Cleef <cleefgulfan312@gmail.com>
2025-07-17 17:20:18 +0100

    Added properties to ProfileDTO.

--- affiliate-proj/Core/DTOs/Account/ProfileDTO.cs
+++ affiliate-proj/Core/DTOs/Account/ProfileDTO.cs
@@ -2,5 +2,14 @@
 
 public class ProfileDTO
 {
-    
+    public Guid UserId { get; set; }
+    public string Username { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public string Email { get; set; }
+    public Guid CreatorId { get; set; }
+    public string Firstname  { get; set; }
+    public string Surname { get; set; }
+    public DateOnly Dob { get; set; }
+    public string? StripeId  { get; set; }
 }
