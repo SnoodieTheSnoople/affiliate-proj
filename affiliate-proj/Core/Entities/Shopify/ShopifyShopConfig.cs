da5304f7179b80d1cdf0d8718eeecb239aec708e
Cleef <cleefgulfan312@gmail.com>
2025-07-21 13:52:41 +0100

    Added Shopify shop config model.

---
+++ affiliate-proj/Core/Entities/Shopify/ShopifyShopConfig.cs
@@ -0,0 +1,8 @@
+namespace affiliate_proj.Core.Entities.Shopify;
+
+public class ShopifyShopConfig
+{
+    public string ShopDomain { get; set; }
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
