bf45b96a555307ae679ba7adff4b891ac5c7da4f
Cleef <cleefgulfan312@gmail.com>
2025-07-21 14:07:21 +0100

    Added interface methods.

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
@@ -2,5 +2,7 @@ namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyAuthService
 {
-    
+    string BuildAuthUrl(string shopDomain, string state);
+    Task<string> AuthorizeAsync(string code, string shopDomain);
+    Task<bool> IsValidWebhookAsync(string requestBody,  string shopifyHmacHeader);
 }
