7b10fac729f518739ebb6f87ff5fb5d9f60ed809
Cleef <cleefgulfan312@gmail.com>
2025-07-23 17:55:38 +0100

    Added missing methods to concrete class.

--- affiliate-proj/Application/Services/Shopify/ShopifyDataService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyDataService.cs
@@ -1,6 +1,32 @@
-﻿namespace affiliate_proj.Application.Services.Shopify;
+﻿using affiliate_proj.Application.Interfaces.Shopify;
+using ShopifySharp;
 
-public class ShopifyDataService
+namespace affiliate_proj.Application.Services.Shopify;
+
+public class ShopifyDataService : IShopifyDataService
 {
-    
+    public Task<IEnumerable<Product>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Order>> GetOrdersAsync(string shopDomain, string accessToken, int limit = 250)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Fulfillment>> GetFulfillmentsAsync(string shopDomain, string accessToken, int limit = 250)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Refund>> GetRefundsAsync(string shopDomain, string accessToken, int limit = 250)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Shop> GetShopAsync(string shopDomain, string accessToken)
+    {
+        throw new NotImplementedException();
+    }
 }
