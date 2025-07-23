1437d279b050c3567cfe009c2ed3f3757d71be91
Cleef <cleefgulfan312@gmail.com>
2025-07-23 14:14:04 +0100

    Implemented AuthorizeAsync

--- affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
@@ -1,5 +1,6 @@
 using affiliate_proj.Application.Interfaces.Shopify;
 using affiliate_proj.Core.Entities.Shopify;
+using Microsoft.AspNetCore.Authentication.BearerToken;
 using ShopifySharp.Utilities;
 
 namespace affiliate_proj.Application.Services.Shopify;
@@ -8,14 +9,14 @@ public class ShopifyAuthService :  IShopifyAuthService
 {
     private readonly ShopifyConfig _shopifyConfig;
     private readonly ILogger<ShopifyAuthService> _logger;
-    private readonly ShopifyDomainUtility _shopifyDomainUtility;
+    private readonly HttpClient _httpClient;
 
     public ShopifyAuthService(ShopifyConfig shopifyConfig, ILogger<ShopifyAuthService> logger,
-        ShopifyDomainUtility shopifyDomainUtility)
+        HttpClient httpClient)
     {
         _shopifyConfig = shopifyConfig;
         _logger = logger;
-        _shopifyDomainUtility = shopifyDomainUtility;
+        _httpClient = httpClient;
     }
     public string BuildAuthUrl(string shopDomain, string state)
     {
@@ -33,9 +34,39 @@ public class ShopifyAuthService :  IShopifyAuthService
         return authUrl;
     }
 
-    public Task<string> AuthorizeAsync(string code, string shopDomain)
+    public async Task<string> AuthorizeAsync(string code, string shopDomain)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var redirectUrl = $"{_shopifyConfig.AppUrl}/auth/callback";
+            var formattedShopDomain = FormatShopDomain(shopDomain);
+
+            var tokenRequestData = new Dictionary<string, string>
+            {
+                { "client_id", _shopifyConfig.ApiKey },
+                { "client_secret", _shopifyConfig.ApiSecret },
+                { "code", code },
+                { "redirect_uri", redirectUrl },
+            };
+
+            var tokenRequestContent = new FormUrlEncodedContent(tokenRequestData);
+            var tokenUrl = $"https://{formattedShopDomain}/admin/oauth/access_token";
+
+            var response = await _httpClient.PostAsync(tokenUrl, tokenRequestContent);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var tokenResponse = System.Text.Json.JsonSerializer.Deserialize<AccessTokenResponse>(responseContent);
+
+            if (tokenResponse.AccessToken == null)
+                throw new InvalidOperationException("No access token found");
+            return tokenResponse.AccessToken;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return ex.Message;
+        }
     }
 
     public Task<bool> IsValidWebhookAsync(string requestBody, string shopifyHmacHeader)
