f9d3b2687c8b1d190086842a04c0bc325c8b3fa5
Cleef <cleefgulfan312@gmail.com>
2025-07-23 12:54:25 +0100

    Added properties and constructor.

--- affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
@@ -1,9 +1,18 @@
 using affiliate_proj.Application.Interfaces.Shopify;
+using affiliate_proj.Core.Entities.Shopify;
 
 namespace affiliate_proj.Application.Services.Shopify;
 
 public class ShopifyAuthService :  IShopifyAuthService
 {
+    private readonly ShopifyConfig _shopifyConfig;
+    private readonly ILogger<ShopifyAuthService> _logger;
+
+    public ShopifyAuthService(ShopifyConfig shopifyConfig, ILogger<ShopifyAuthService> logger)
+    {
+        _shopifyConfig = shopifyConfig;
+        _logger = logger;
+    }
     public string BuildAuthUrl(string shopDomain, string state)
     {
         throw new NotImplementedException();
