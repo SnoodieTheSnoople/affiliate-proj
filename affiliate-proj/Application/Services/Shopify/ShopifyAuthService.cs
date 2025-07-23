72caa335ae2498fe043e443f492afe465af31b62
Cleef <cleefgulfan312@gmail.com>
2025-07-23 12:53:39 +0100

    Added identifier to extend interface class.

--- affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
@@ -1,6 +1,21 @@
+using affiliate_proj.Application.Interfaces.Shopify;
+
 namespace affiliate_proj.Application.Services.Shopify;
 
-public class ShopifyAuthService
+public class ShopifyAuthService :  IShopifyAuthService
 {
-    
+    public string BuildAuthUrl(string shopDomain, string state)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> AuthorizeAsync(string code, string shopDomain)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsValidWebhookAsync(string requestBody, string shopifyHmacHeader)
+    {
+        throw new NotImplementedException();
+    }
 }
