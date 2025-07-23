f1109ee7f563bbd0e5b29bf10449a7e5762f6930
Cleef <cleefgulfan312@gmail.com>
2025-07-23 17:52:47 +0100

    Added interface methods.

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
@@ -1,6 +1,13 @@
+using System.Collections;
+using ShopifySharp;
+
 namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyDataService
 {
-    
+    Task<IEnumerable<Product>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250);
+    Task<IEnumerable<Order>> GetOrdersAsync(string shopDomain, string accessToken, int limit = 250);
+    Task<IEnumerable<Fulfillment>> GetFulfillmentsAsync(string shopDomain, string accessToken, int limit = 250);
+    Task<IEnumerable<Refund>> GetRefundsAsync(string shopDomain, string accessToken, int limit = 250);
+    Task<Shop> GetShopAsync(string shopDomain, string accessToken);
 }
