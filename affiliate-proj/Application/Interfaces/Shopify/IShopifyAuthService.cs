4cee45b3d7eb865a2bcfd1a8a049827fa7d11fbe
Cleef <cleefgulfan312@gmail.com>
2025-08-06 00:00:14 +0100

    Added new methods extracted from controller

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
@@ -1,6 +1,10 @@
+using ShopifySharp;
+
 namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyAuthService
 {
+    Task<string> GenerateInstallUrlAsync(string shop);
+    Task<AuthorizationResult> HandleCallbackAsync(string code, string shop, string state);
     
 }
--- affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
@@ -1,8 +1,17 @@
 using affiliate_proj.Application.Interfaces.Shopify;
+using ShopifySharp;
 
 namespace affiliate_proj.Application.Services.Shopify;
 
 public class ShopifyAuthService :  IShopifyAuthService
 {
-    
+    public Task<string> GenerateInstallUrlAsync(string shop)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AuthorizationResult> HandleCallbackAsync(string code, string shop, string state)
+    {
+        throw new NotImplementedException();
+    }
 }
