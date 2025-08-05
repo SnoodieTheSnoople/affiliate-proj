5e48b06f3c2b2230191683dc80da733f8652f29a
Cleef <cleefgulfan312@gmail.com>
2025-08-06 00:03:38 +0100

    Added necessary properties and constructor

--- affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyAuthService.cs
@@ -1,10 +1,31 @@
 using affiliate_proj.Application.Interfaces.Shopify;
+using Microsoft.Extensions.Caching.Memory;
 using ShopifySharp;
+using ShopifySharp.Utilities;
 
 namespace affiliate_proj.Application.Services.Shopify;
 
 public class ShopifyAuthService :  IShopifyAuthService
 {
+    private readonly IConfiguration _configuration;
+    private readonly IShopifyRequestValidationUtility _shopifyRequestValidationUtility;
+    private readonly IShopifyDomainUtility _shopifyDomainUtility;
+    private readonly IShopifyOauthUtility _shopifyOauthUtility;
+    private readonly IMemoryCache _memoryCache;
+    private readonly ILogger<ShopifyAuthService> _logger;
+
+    public ShopifyAuthService(IConfiguration configuration,
+        IShopifyRequestValidationUtility shopifyRequestValidationUtility,
+        IShopifyDomainUtility shopifyDomainUtility, IShopifyOauthUtility shopifyOauthUtility,
+        IMemoryCache memoryCache, ILogger<ShopifyAuthService> logger)
+    {
+        _configuration = configuration;
+        _shopifyRequestValidationUtility = shopifyRequestValidationUtility;
+        _shopifyDomainUtility = shopifyDomainUtility;
+        _shopifyOauthUtility = shopifyOauthUtility;
+        _memoryCache = memoryCache;
+        _logger = logger;
+    }
     public Task<string> GenerateInstallUrlAsync(string shop)
     {
         throw new NotImplementedException();
