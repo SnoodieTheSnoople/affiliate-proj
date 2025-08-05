b25d5fbb9d1799931cdf7e038069e0c71fb9c668
Cleef <cleefgulfan312@gmail.com>
2025-08-05 23:53:40 +0100

    Removed unused methods

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyAuthService.cs
@@ -2,7 +2,5 @@ namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyAuthService
 {
-    string BuildAuthUrl(string shopDomain, string state);
-    Task<string> AuthoriseAsync(string code, string shopDomain);
-    Task<bool> IsValidWebhookAsync(string requestBody,  string shopifyHmacHeader);
+    
 }
