59b45a939c03d3f44eb6147f4c455c0406f3d8c5
Cleef <cleefgulfan312@gmail.com>
2025-08-08 19:51:29 +0100

    Changed return type for GetProductsAsync

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
@@ -1,11 +1,14 @@
 using System.Collections;
+using affiliate_proj.Core.DataTypes;
 using ShopifySharp;
+using ShopifySharp.Lists;
+using ShopifySharp.Services.Graph;
 
 namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyDataService
 {
-    Task<IEnumerable<Product>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250);
+    Task<GraphResult<ListProductsResult>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Order>> GetOrdersAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Fulfillment>> GetFulfillmentsAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Refund>> GetRefundsAsync(string shopDomain, string accessToken, int limit = 250);
