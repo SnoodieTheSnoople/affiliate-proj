24aa7cbb8db60c718ee39710210fbd6a44abb6da
Cleef <cleefgulfan312@gmail.com>
2025-08-08 19:54:03 +0100

    Implemented GetProductsAsync using ShopifySharp GQL

--- affiliate-proj/Application/Services/Shopify/ShopifyDataService.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyDataService.cs
@@ -1,13 +1,65 @@
 ﻿using affiliate_proj.Application.Interfaces.Shopify;
+using affiliate_proj.Core.DataTypes;
 using ShopifySharp;
+using ShopifySharp.GraphQL;
+using ShopifySharp.Services.Graph;
+using Fulfillment = ShopifySharp.Fulfillment;
+using Order = ShopifySharp.Order;
+using Refund = ShopifySharp.Refund;
+using Shop = ShopifySharp.Shop;
 
 namespace affiliate_proj.Application.Services.Shopify;
 
 public class ShopifyDataService : IShopifyDataService
 {
-    public Task<IEnumerable<Product>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250)
+    public async Task<GraphResult<ListProductsResult>> GetProductsAsync(string shopDomain, string accessToken,
+        int limit = 250)
     {
-        throw new NotImplementedException();
+        var graphSerivce = new GraphService(shopDomain, accessToken);
+        var request = new GraphRequest
+        {
+            Query =
+                """
+                query listProducts($limit: Int!, $query: String!) {
+                    products(first: $limit, query: $query) {
+                        pageInfo {
+                            startCursor
+                            endCursor
+                            hasNextPage
+                            hasPreviousPage
+                        }
+                        nodes {
+                            id
+                            title
+                            handle
+                            hasOnlyDefaultVariant
+                            variantsCount {
+                                count
+                            }
+                        }
+                    }
+                }
+                """,
+            Variables = new Dictionary<string, object>
+            {
+                { "limit", 3 },
+                { "query", "status:" + ProductStatus.ACTIVE }
+            }
+        };
+        
+        var graphResult = await graphSerivce.PostAsync<ListProductsResult>(request);
+
+        foreach (var node in graphResult.Data.Products.nodes)
+        {
+            if (node.id is not null)
+                Console.WriteLine("Product ID is: " + node.id);
+        }
+        
+        if ((bool)graphResult.Data.Products.pageInfo.hasNextPage)
+            Console.WriteLine("Another Page of products available with cursor:" + 
+                              graphResult.Data.Products.pageInfo.endCursor);
+        
+        return graphResult;
     }
 
     public Task<IEnumerable<Order>> GetOrdersAsync(string shopDomain, string accessToken, int limit = 250)
