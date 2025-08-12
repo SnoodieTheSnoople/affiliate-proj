6226a381c9e47f928d186dff0406a929c5afe2eb
Cleef <cleefgulfan312@gmail.com>
2025-08-12 21:39:56 +0100

    Created StoreDTO

---
+++ affiliate-proj/Core/DTOs/Account/StoreDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace affiliate_proj.Core.DTOs.Account;
+
+public class StoreDTO
+{
+    public Guid StoreId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? StoreName { get; set; }
+    public string ShopifyId { get; set; }
+    public string ShopifyToken { get; set; }
+    public string StoreUrl { get; set; }
+    public string ShopifyStoreName { get; set; }
+    public string ShopifyOwnerName { get; set; }
+    public string ShopifyOwnerEmail { get; set; }
+    public string ShopifyOwnerPhone { get; set; }
+    public string ShopifyCountry { get; set; }
+}
