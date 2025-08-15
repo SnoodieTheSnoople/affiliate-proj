030cbd0c7c87a16cd625b468add1bf50e088c24e
Cleef <cleefgulfan312@gmail.com>
2025-08-15 20:15:19 +0100

    Moved SetStoreProfile to StoresController.cs

--- affiliate-proj/API/Controllers/Account/StoresController.cs
+++ affiliate-proj/API/Controllers/Account/StoresController.cs
@@ -1,3 +1,5 @@
+using affiliate_proj.Core.DTOs.Account;
+using affiliate_proj.Core.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +9,10 @@ namespace affiliate_proj.API.Controllers.Account
     [ApiController]
     public class StoresController : ControllerBase
     {
+        [HttpPost("/set-store")]
+        public async Task<ActionResult<Store>> SetStoreProfile([FromBody] StoreDTO request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
