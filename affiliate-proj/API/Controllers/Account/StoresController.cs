cc0d9b2bf4d3cd3b7525491aa663b4e039a87631
Cleef <cleefgulfan312@gmail.com>
2025-08-15 20:13:30 +0100

    Added Stores controller as part of Accounts

---
+++ affiliate-proj/API/Controllers/Account/StoresController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Controllers.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController : ControllerBase
+    {
+    }
+}
