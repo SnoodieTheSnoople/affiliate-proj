fba84c4daef5cf50f83100cfc524f806141b9baa
cleef <cleefgulfan312@gmail.com>
2025-08-18 19:13:26 +0100

    Refactor - Changedd parameter from StoreDTO -> Store

--- affiliate-proj/API/Controllers/Account/StoresController.cs
+++ affiliate-proj/API/Controllers/Account/StoresController.cs
@@ -26,7 +26,7 @@ namespace affiliate_proj.API.Controllers.Account
         }
         
         [HttpPost("set-store")]
-        public async Task<ActionResult<Store>> SetStoreProfileAsync([FromBody] StoreDTO request)
+        public async Task<ActionResult<Store>> SetStoreProfileAsync([FromBody] Store request)
         {
             if (request.UserId == Guid.Empty || String.IsNullOrEmpty(request.StoreName) 
                                              || request.ShopifyId == null 
