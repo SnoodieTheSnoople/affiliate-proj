9f1ee9c0d034f27b9cfcad8f2404ac3a021050d2
cleef <cleefgulfan312@gmail.com>
2025-08-18 19:13:06 +0100

    Refactor - Updated StoreDTO to retain exposable properties

--- affiliate-proj/Core/DTOs/Account/StoreDTO.cs
+++ affiliate-proj/Core/DTOs/Account/StoreDTO.cs
@@ -5,14 +5,6 @@ public class StoreDTO
     public Guid StoreId { get; set; }
     public DateTime CreatedAt { get; set; }
     public string? StoreName { get; set; }
-    public long ShopifyId { get; set; }
-    public string ShopifyToken { get; set; }
     public string StoreUrl { get; set; }
-    public string ShopifyStoreName { get; set; }
-    public string ShopifyOwnerName { get; set; }
-    public string ShopifyOwnerEmail { get; set; }
-    public string? ShopifyOwnerPhone { get; set; }
     public string ShopifyCountry { get; set; }
-    public string? ShopifyGrantedScopes { get; set; }
-    public Guid UserId { get; set; }
 }
