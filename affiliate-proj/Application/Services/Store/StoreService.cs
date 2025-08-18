d0d2de0ed29b9331ab57262544cdc5b3a43100d6
cleef <cleefgulfan312@gmail.com>
2025-08-18 16:59:12 +0100

    Added DbContext and constructor

--- affiliate-proj/Application/Services/Store/StoreService.cs
+++ affiliate-proj/Application/Services/Store/StoreService.cs
@@ -1,9 +1,17 @@
+using affiliate_proj.Accessors.DatabaseAccessors;
 using affiliate_proj.Application.Interfaces.Store;
 
 namespace affiliate_proj.Application.Services.Store;
 
 public class StoreService : IStoreService
 {
+    private readonly PostgresDbContext _postgresDbContext;
+
+    public StoreService(PostgresDbContext postgresDbContext)
+    {
+        _postgresDbContext = postgresDbContext;
+    }
+    
     public Task<Core.Entities.Store> GetAllStoresAsync()
     {
         throw new NotImplementedException();
