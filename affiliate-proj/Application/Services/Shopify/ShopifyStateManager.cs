206678c8a22eec40bfb7e35fbbc3b1c9169b870a
cleef <cleefgulfan312@gmail.com>
2025-08-20 15:40:50 +0100

    Added memoryCache

--- affiliate-proj/Application/Services/Shopify/ShopifyStateManager.cs
+++ affiliate-proj/Application/Services/Shopify/ShopifyStateManager.cs
@@ -1,6 +1,13 @@
+using Microsoft.Extensions.Caching.Memory;
+
 namespace affiliate_proj.Application.Services.Shopify;
 
 public class ShopifyStateManager
 {
-    
+    private readonly IMemoryCache _memoryCache;
+
+    public ShopifyStateManager(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
 }
