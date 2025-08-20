5d76f32b91af8d4264e7ec93cea85ac953a6e4ff
cleef <cleefgulfan312@gmail.com>
2025-08-20 14:41:05 +0100

    Added properties to request model

--- affiliate-proj/Core/RequestModels/SetStoreRequest.cs
+++ affiliate-proj/Core/RequestModels/SetStoreRequest.cs
@@ -2,5 +2,8 @@ namespace affiliate_proj.Core.RequestModels;
 
 public class SetStoreRequest
 {
-    
+    public Guid StoreId { get; set; }
+    public string? StoreName { get; set; }
+    public Guid UserId { get; set; }
+    public bool IsActive { get; set; }
 }
