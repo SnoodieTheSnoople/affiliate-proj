e0a80b9310df18dc0bf037566cbdc1987fe37e6e
cleef <cleefgulfan312@gmail.com>
2025-08-21 16:43:23 +0100

    Added WebhookService as dependency

--- affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookService.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookService.cs
@@ -1,8 +1,14 @@
 using affiliate_proj.Application.Interfaces.Shopify.Webhook;
+using ShopifySharp;
 
 namespace affiliate_proj.Application.Services.Shopify.Webhook;
 
 public class ShopifyWebhookService : IShopifyWebhookService
 {
-    
+    private readonly IWebhookService _webhookService;
+
+    public ShopifyWebhookService(IWebhookService webhookService)
+    {
+        _webhookService = webhookService;
+    }
 }
