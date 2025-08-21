12190cbfc996158e8651d9f1e09fe7ea0e96ea3c
cleef <cleefgulfan312@gmail.com>
2025-08-21 16:34:15 +0100

    Added ShopifyWebhookController

---
+++ affiliate-proj/API/Webhooks/Shopify/ShopifyWebhookController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Webhooks.Shopify
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopifyWebhookController : ControllerBase
+    {
+        
+    }
+}
