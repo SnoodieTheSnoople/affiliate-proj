0ffc81a5a4f90ee7aef8dbe3b4711572d6ade3c6
cleef <cleefgulfan312@gmail.com>
2025-08-21 17:48:40 +0100

    Added properties to reflect table

--- affiliate-proj/Core/Entities/WebhookRegistration.cs
+++ affiliate-proj/Core/Entities/WebhookRegistration.cs
@@ -1,6 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace affiliate_proj.Core.Entities;
 
 public class WebhookRegistration
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid WebhookId { get; set; }
     
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    
+    [Required]
+    public string StoreUrl { get; set; }
+    
+    [Required]
+    public long ShopifyWebhookId { get; set; }
+    
+    [Required]
+    public string Topic { get; set; }
+    
+    [Required]
+    public string Format { get; set; }
+    
+    [Required]
+    public DateTime RegisteredAt { get; set; }
+    
+    [Required]
+    public Guid StoreId { get; set; }
+    
+    [ForeignKey("StoreId")]
+    public Store Store { get; set; }
 }
