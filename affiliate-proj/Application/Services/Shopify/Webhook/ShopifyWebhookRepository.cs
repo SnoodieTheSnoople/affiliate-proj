e04312949214db90c372692a0824fb72927a7923
cleef <cleefgulfan312@gmail.com>
2025-08-21 20:12:45 +0100

    Added dbContext as dependency

--- affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
@@ -1,3 +1,4 @@
+using affiliate_proj.Accessors.DatabaseAccessors;
 using affiliate_proj.Application.Interfaces.Shopify.Webhook;
 using affiliate_proj.Core.DTOs.Account;
 
@@ -5,6 +6,12 @@ namespace affiliate_proj.Application.Services.Shopify.Webhook;
 
 public class ShopifyWebhookRepository : IShopifyWebhookRepository
 {
+    private readonly PostgresDbContext _dbContext;
+
+    public ShopifyWebhookRepository(PostgresDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public Task<CreateWebhookRegistrationDTO> SetShopifyWebhook(CreateWebhookRegistrationDTO registration)
     {
         throw new NotImplementedException();
