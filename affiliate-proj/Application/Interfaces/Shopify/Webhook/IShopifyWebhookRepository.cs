648367a2591f3d54838b6ca376cf37b2f079faf6
cleef <cleefgulfan312@gmail.com>
2025-08-21 20:00:04 +0100

    Added method SetShopifyWebhook

--- affiliate-proj/Application/Interfaces/Shopify/Webhook/IShopifyWebhookRepository.cs
+++ affiliate-proj/Application/Interfaces/Shopify/Webhook/IShopifyWebhookRepository.cs
@@ -1,6 +1,8 @@
+using affiliate_proj.Core.DTOs.Account;
+
 namespace affiliate_proj.Application.Interfaces.Shopify.Webhook;
 
 public interface IShopifyWebhookRepository
 {
-    
+    Task<CreateWebhookRegistrationDTO> SetShopifyWebhook(CreateWebhookRegistrationDTO registration);
 }
--- affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
@@ -1,8 +1,12 @@
 using affiliate_proj.Application.Interfaces.Shopify.Webhook;
+using affiliate_proj.Core.DTOs.Account;
 
 namespace affiliate_proj.Application.Services.Shopify.Webhook;
 
 public class ShopifyWebhookRepository : IShopifyWebhookRepository
 {
-    
+    public Task<CreateWebhookRegistrationDTO> SetShopifyWebhook(CreateWebhookRegistrationDTO registration)
+    {
+        throw new NotImplementedException();
+    }
 }
