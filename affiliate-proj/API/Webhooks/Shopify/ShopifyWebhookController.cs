393efe665d47e99f9a07f9ccc7ea29fdf177204b
cleef <cleefgulfan312@gmail.com>
2025-08-21 19:11:13 +0100

    Basic implementation of SetWebhookAsync and added ShopifyWebhookService as dependency

--- affiliate-proj/API/Webhooks/Shopify/ShopifyWebhookController.cs
+++ affiliate-proj/API/Webhooks/Shopify/ShopifyWebhookController.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using affiliate_proj.Application.Interfaces.Shopify.Webhook;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShopifySharp;
 
 namespace affiliate_proj.API.Webhooks.Shopify
 {
@@ -7,10 +10,33 @@ namespace affiliate_proj.API.Webhooks.Shopify
     [ApiController]
     public class ShopifyWebhookController : ControllerBase
     {
+        private readonly IShopifyWebhookService _shopifyWebhookService;
+
+        public ShopifyWebhookController(IShopifyWebhookService shopifyWebhookService)
+        {
+            _shopifyWebhookService = shopifyWebhookService;
+        }
+        
         [HttpPost("app/uninstalled")]
         public async Task<IActionResult> AppUninstalledAsync()
         {
             throw new NotImplementedException();
+        }
+
+        [HttpPost("set-webhook")]
+        public async Task<IActionResult> SetWebhookAsync([FromQuery] string shop, [FromQuery] string accessToken)
+        {
+            try
+            {
+                await _shopifyWebhookService.RegisterWebhookAsync(shop, accessToken);
+                await _shopifyWebhookService.GetAllWebhooksAsync(shop, accessToken);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
         }
     }
 }
