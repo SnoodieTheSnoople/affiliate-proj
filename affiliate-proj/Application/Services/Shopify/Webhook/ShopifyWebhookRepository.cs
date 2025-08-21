7d90b3b6f0e84141a9af0a7fefa7a1e1a75b3901
cleef <cleefgulfan312@gmail.com>
2025-08-21 19:58:47 +0100

    Extended ShopifyWebhookRepository to interface

--- affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookRepository.cs
@@ -1,6 +1,8 @@
+using affiliate_proj.Application.Interfaces.Shopify.Webhook;
+
 namespace affiliate_proj.Application.Services.Shopify.Webhook;
 
-public class ShopifyWebhookRepository
+public class ShopifyWebhookRepository : IShopifyWebhookRepository
 {
     
 }
