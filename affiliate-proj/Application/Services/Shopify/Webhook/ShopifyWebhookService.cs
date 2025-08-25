8db99eec18305009292bc634fb15a26aab922605
Cleef <cleefgulfan312@gmail.com>
2025-08-25 20:01:11 +0100

    Verified if added webhooks are read correctly

--- affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookService.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/ShopifyWebhookService.cs
@@ -63,6 +63,8 @@ public class ShopifyWebhookService : IShopifyWebhookService
     
     public async Task<ListResult<ShopifySharp.Webhook>> GetAllWebhooksAsync(string shop, string accessToken)
     {
+        var listOfWebhooksRegistration = _configuration.GetSection("Shopify:Webhooks").Get<List<string>>();
+        Console.WriteLine(String.Join(", ", listOfWebhooksRegistration));
         var webhookService = new WebhookService(shop, accessToken);
         var webhooksEnumerable = await webhookService.ListAsync();
         return webhooksEnumerable;
