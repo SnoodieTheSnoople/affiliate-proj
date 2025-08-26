8bf4b7a208a91df58cfc38fc3eb413202d041789
Cleef <cleefgulfan312@gmail.com>
2025-08-26 13:39:07 +0100

    Added RegisterWebhooksAsync

--- affiliate-proj/Application/Interfaces/Shopify/Webhook/IShopifyWebhookService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/Webhook/IShopifyWebhookService.cs
@@ -6,6 +6,7 @@ namespace affiliate_proj.Application.Interfaces.Shopify.Webhook;
 public interface IShopifyWebhookService
 {
     Task RegisterWebhookAsync(string shop, string accessToken);
+    Task RegisterWebhooksAsync(string shop, string accessToken);
     Task<ListResult<ShopifySharp.Webhook>> GetAllWebhooksAsync(string shop, string accessToken);
     Task UpdateAllWebhookAsync(string shop, string accessToken, long webhookId);
     Task RemoveWebhookAsync(string shop, string accessToken, long webhookId);
