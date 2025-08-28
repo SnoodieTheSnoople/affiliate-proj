a151a0c805e6b37b17a0ce04f38c7eb0e9f0805d
Cleef <cleefgulfan312@gmail.com>
2025-08-28 17:44:00 +0100

    Added SetCommissionRate method

--- affiliate-proj/API/Controllers/Account/CreatorCommissionsController.cs
+++ affiliate-proj/API/Controllers/Account/CreatorCommissionsController.cs
@@ -1,6 +1,19 @@
-﻿namespace affiliate_proj.API.Controllers.Account;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Controllers.Account;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
 
 public class CreatorCommissionsController
 {
+    [HttpPost("set-commission-rate")]
+    public async Task<IActionResult> SetCommissionRate()
+    {
+        throw new NotImplementedException();
+    }
+    
     
 }
