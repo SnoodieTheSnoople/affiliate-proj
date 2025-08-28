1cb04d9e660f1ac780dae74783bc59dd55a22b93
Cleef <cleefgulfan312@gmail.com>
2025-08-28 13:51:41 +0100

    Added GraphServiceFactory to be used in ShopifyDataService

---
+++ affiliate-proj/Application/Interfaces/Shopify/Data/Factories/IGraphServiceFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace affiliate_proj.Application.Interfaces.Shopify.Data.Factories;
+
+public interface IGraphServiceFactory
+{
+    
+}
---
+++ affiliate-proj/Application/Services/Shopify/Data/Factories/GraphServiceFactory.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.Shopify.Data.Factories;
+
+namespace affiliate_proj.Application.Services.Shopify.Data.Factories;
+
+public class GraphServiceFactory : IGraphServiceFactory
+{
+    
+}
