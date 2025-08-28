492f91f0a9d86a446398bdea4141c19f665fad40
Cleef <cleefgulfan312@gmail.com>
2025-08-28 17:07:35 +0100

    Added custom data type for GQL

---
+++ affiliate-proj/Core/DataTypes/GraphQL/CustomListProductsResult.cs
@@ -0,0 +1,51 @@
+﻿using ShopifySharp.GraphQL;
+
+namespace affiliate_proj.Core.DataTypes.GraphQL;
+
+public class CustomListProductsResult
+{
+    public CustomProductsConnection Products { get; set; }
+}
+
+public class CustomProductsConnection
+{
+    public PageInfo PageInfo { get; set; }
+    public List<CustomProductNode> Nodes { get; set; }
+}
+
+public class CustomProductNode
+{
+    public string id { get; set; }
+    public string Title { get; set; }
+    public string Handle { get; set; }
+    public bool HasOnlyDefaultVariant { get; set; }
+    public string OnlineStoreUrl { get; set; }
+    public CustomMediaConnection Media { get; set; }
+}
+
+public class CustomMediaConnection
+{
+    public List<CustomMediaNode> Nodes { get; set; }
+}
+
+public class CustomMediaNode
+{
+    public string Id { get; set; }
+    public string MediaContentType { get; set; }
+    public string Alt { get; set; }
+    public CustomPreview Preview { get; set; }
+}
+
+public class CustomPreview
+{
+    public CustomImage Image { get; set; }
+}
+
+public class CustomImage
+{
+    public string Id { get; set; }
+    public string AltText { get; set; }
+    public int? Height { get; set; }
+    public int? Width { get; set; }
+    public string Url { get; set; }
+}
