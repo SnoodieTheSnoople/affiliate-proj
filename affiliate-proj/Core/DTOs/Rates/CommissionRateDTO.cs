925dff6c32dfbe93cc8b98c7f16cb797febc6ac9
Cleef <cleefgulfan312@gmail.com>
2025-09-01 17:56:24 +0100

    Added properties to CommissionRateDTO.cs

--- affiliate-proj/Core/DTOs/Rates/CommissionRateDTO.cs
+++ affiliate-proj/Core/DTOs/Rates/CommissionRateDTO.cs
@@ -2,5 +2,10 @@
 
 public class CommissionRateDTO
 {
-    
+    public string RateId {  get; set; }
+    public string CreatorId { get; set; }
+    public string StoreId { get; set; }
+    public float Rate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsAccepted { get; set; }
 }
