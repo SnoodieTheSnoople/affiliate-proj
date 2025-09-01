5e4c11cdfdc2b4c059e5b89c30ca5e7640117d2b
Cleef <cleefgulfan312@gmail.com>
2025-09-01 20:42:31 +0100

    Implemented SetCommissionRateAsync, changed return to CommissionRateDTO

--- affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
+++ affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
@@ -14,15 +14,33 @@ public class CommissionRatesRepository
         _postgresDbContext = postgresDbContext;
     }
 
-    public async Task<CreateCommissionRateDTO> SetCommissionRateAsync(CommissionRate commissionRate)
+    public async Task<CommissionRateDTO> SetCommissionRateAsync(CommissionRate commissionRate)
     {
-        var checkRateExists = _postgresDbContext.CommissionRates
+        var checkRateExists = await _postgresDbContext.CommissionRates
             .Where(rate => rate.CreatorId == commissionRate.CreatorId)
             .Where(rate => rate.StoreId == commissionRate.StoreId)
             .FirstOrDefaultAsync();
         
         if (checkRateExists != null)
             throw new Exception("Commission rate already exists");
+        
+        await _postgresDbContext.CommissionRates.AddAsync(commissionRate);
+        await _postgresDbContext.SaveChangesAsync();
+        
+        checkRateExists = await _postgresDbContext.CommissionRates
+            .Where(rate => rate.CreatorId == commissionRate.CreatorId)
+            .Where(rate => rate.StoreId == commissionRate.StoreId)
+            .FirstOrDefaultAsync();
+
+        return new CommissionRateDTO
+        {
+            RateId = checkRateExists.RateId,
+            CreatedAt = checkRateExists.CreatedAt,
+            CreatorId = checkRateExists.CreatorId,
+            StoreId = checkRateExists.StoreId,
+            Rate = checkRateExists.Rate,
+            IsAccepted = checkRateExists.IsAccepted,
+        };
         
         throw new NotImplementedException();
     }
