943f81f7e071d0a401acd56abebc3a4775612660
Cleef <cleefgulfan312@gmail.com>
2025-09-01 18:24:47 +0100

    Addedd SetCommissionRateAsync to CommissionRatesRepository.cs

--- affiliate-proj/Application/Interfaces/Account/Rates/ICommissionRatesRepository.cs
+++ affiliate-proj/Application/Interfaces/Account/Rates/ICommissionRatesRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Account.Rates;
+﻿using affiliate_proj.Core.DTOs.Rates;
+
+namespace affiliate_proj.Application.Interfaces.Account.Rates;
 
 public interface ICommissionRatesRepository
 {
-    
+    Task<CreateCommissionRateDTO> SetCommissionRateAsync(CommissionRateDTO commissionRate);
 }
--- affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
+++ affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace affiliate_proj.Application.Services.Account.Rates;
+﻿using affiliate_proj.Core.DTOs.Rates;
+
+namespace affiliate_proj.Application.Services.Account.Rates;
 
 public class CommissionRatesRepository
 {
-    
+    public async Task<CreateCommissionRateDTO> SetCommissionRateAsync(CommissionRateDTO commissionRate)
+    {
+        throw new NotImplementedException();
+    }
 }
