fb745f2842b998bc5f92a15d46474cf1720fdc46
Cleef <cleefgulfan312@gmail.com>
2025-09-01 17:45:26 +0100

    Added decorations such as keys, not null, to entity

--- affiliate-proj/Core/Entities/CommissionRate.cs
+++ affiliate-proj/Core/Entities/CommissionRate.cs
@@ -1,11 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace affiliate_proj.Core.Entities;
 
 public class CommissionRate
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string RateId {  get; set; }
+    
+    [Required]
     public string CreatorId { get; set; }
+    
+    [ForeignKey(nameof(CreatorId))]
+    public Creator Creator { get; set; }
+    
+    [Required]
     public string StoreId { get; set; }
+    
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
+    
+    [Required]
     public float Rate { get; set; }
+    
     public DateTime CreatedAt { get; set; }
+    
+    [Required]
     public bool IsAccepted { get; set; }
 }
