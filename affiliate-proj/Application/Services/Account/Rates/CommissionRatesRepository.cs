ca7d8102a413988d61441c2bf05508989697cac4
Cleef <cleefgulfan312@gmail.com>
2025-09-01 18:25:42 +0100

    Added PostgresDbContext

--- affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
+++ affiliate-proj/Application/Services/Account/Rates/CommissionRatesRepository.cs
@@ -1,9 +1,17 @@
-﻿using affiliate_proj.Core.DTOs.Rates;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Core.DTOs.Rates;
 
 namespace affiliate_proj.Application.Services.Account.Rates;
 
 public class CommissionRatesRepository
 {
+    private readonly PostgresDbContext _postgresDbContext;
+
+    public CommissionRatesRepository(PostgresDbContext postgresDbContext)
+    {
+        _postgresDbContext = postgresDbContext;
+    }
+
     public async Task<CreateCommissionRateDTO> SetCommissionRateAsync(CommissionRateDTO commissionRate)
     {
         throw new NotImplementedException();
