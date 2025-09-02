8e6e470c334d2ef689fe8f2841e58a7335692829
Cleef <cleefgulfan312@gmail.com>
2025-09-02 14:05:39 +0100

    Added unit testing class and project

---
+++ affiliate-proj.Tests/Application/Services/Account/Rates/CommissionRatesServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using affiliate_proj.Application.Services.Account.Rates;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace affiliate_proj.Tests.Application.Services.Account.Rates;
+
+[TestSubject(typeof(CommissionRatesService))]
+public class CommissionRatesServiceTest
+{
+    
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
