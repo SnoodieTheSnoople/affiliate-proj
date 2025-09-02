14aeecc85e3ebeb212278ca6d4e9b584c9bea7b1
Cleef <cleefgulfan312@gmail.com>
2025-09-02 14:08:07 +0100

    Added dependencies to CommissionRatesServiceTest.cs

--- affiliate-proj.Tests/Application/Services/Account/Rates/CommissionRatesServiceTest.cs
+++ affiliate-proj.Tests/Application/Services/Account/Rates/CommissionRatesServiceTest.cs
@@ -1,5 +1,7 @@
-﻿using affiliate_proj.Application.Services.Account.Rates;
+﻿using affiliate_proj.Application.Interfaces.Account.Rates;
+using affiliate_proj.Application.Services.Account.Rates;
 using JetBrains.Annotations;
+using Moq;
 using Xunit;
 
 namespace affiliate_proj.Tests.Application.Services.Account.Rates;
@@ -7,7 +9,14 @@ namespace affiliate_proj.Tests.Application.Services.Account.Rates;
 [TestSubject(typeof(CommissionRatesService))]
 public class CommissionRatesServiceTest
 {
-    
+    private readonly Mock<ICommissionRatesRepository> _commissionRatesRepository;
+    private readonly CommissionRatesService _commissionRatesService;
+
+    public CommissionRatesServiceTest(Mock<ICommissionRatesRepository> commissionRatesRepository)
+    {
+        _commissionRatesRepository = commissionRatesRepository;
+        _commissionRatesService = new CommissionRatesService(_commissionRatesRepository.Object);
+    }
 
     [Fact]
     public void METHOD()
