8a17925eb8ee44feffa54231b54b221a6e3cd7d2
Cleef <cleefgulfan312@gmail.com>
2025-09-04 17:21:34 +0100

    Removed moved methods, unused directives, unused dependencies

--- affiliate-proj/API/Controllers/Shopify/ShopifyController.cs
+++ affiliate-proj/API/Controllers/Shopify/ShopifyController.cs
@@ -1,108 +1,74 @@
-using affiliate_proj.Application.Interfaces;
 using affiliate_proj.Application.Interfaces.Account;
 using affiliate_proj.Application.Interfaces.Shopify;
 using affiliate_proj.Application.Interfaces.Store;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using ShopifySharp.Enums;
 
-namespace affiliate_proj.API.Controllers.Shopify
+namespace affiliate_proj.API.Controllers.Shopify;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShopifyController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ShopifyController : ControllerBase
+    private readonly IShopifyAuthService _shopifyAuthService;
+    private readonly IShopifyStoreHelper _shopifyStoreHelper;
+    private readonly IAccountHelper _accountHelper;
+
+    public ShopifyController(IShopifyAuthService shopifyAuthService,
+        IShopifyStoreHelper shopifyStoreHelper, IAccountHelper accountHelper)
     {
-        private readonly IShopifyAuthService _shopifyAuthService;
-        private readonly IShopifyDataService _shopifyDataService;
-        private readonly IShopifyStoreHelper _shopifyStoreHelper;
-        private readonly IAccountHelper _accountHelper;
-
-        public ShopifyController(IShopifyAuthService shopifyAuthService, IShopifyDataService shopifyDataService,
-            IShopifyStoreHelper shopifyStoreHelper, IAccountHelper accountHelper)
-        {
-            _shopifyAuthService = shopifyAuthService;
-            _shopifyDataService = shopifyDataService;
-            _shopifyStoreHelper = shopifyStoreHelper;
-            _accountHelper = accountHelper;
-        }
+        _shopifyAuthService = shopifyAuthService;
+        _shopifyStoreHelper = shopifyStoreHelper;
+        _accountHelper = accountHelper;
+    }
         
-        [HttpGet("install")]
-        public async Task<IActionResult> Install([FromQuery] string shop)
+    [HttpGet("install")]
+    public async Task<IActionResult> Install([FromQuery] string shop)
+    {
+        try
         {
-            try
-            {
-                var userId = _accountHelper.GetUserIdFromAccessToken();
-                if (String.IsNullOrEmpty(userId)) 
-                    return Unauthorized();
+            var userId = _accountHelper.GetUserIdFromAccessToken();
+            if (String.IsNullOrEmpty(userId)) 
+                return Unauthorized();
 
-                if (!await _accountHelper.CheckUserIsStoreOwnerAsync(Guid.Parse(userId)))
-                    return Unauthorized();
+            if (!await _accountHelper.CheckUserIsStoreOwnerAsync(Guid.Parse(userId)))
+                return Unauthorized();
                 
-                var redirectUrl = await _shopifyAuthService.GenerateInstallUrlWithUserIdAsync(shop, 
-                    Guid.Parse(userId));
+            var redirectUrl = await _shopifyAuthService.GenerateInstallUrlWithUserIdAsync(shop, 
+                Guid.Parse(userId));
                 
-                return Ok(redirectUrl);
-            }
-            catch (Exception e)
-            {
-                // TODO: Create exception handling system according to custom codes.
-                Console.WriteLine(e);
-                return BadRequest(e.Message);
-            }
+            return Ok(redirectUrl);
         }
-
-        [HttpGet("callback")]
-        public async Task<IActionResult> Callback([FromQuery] string code, [FromQuery] string shop, 
-            [FromQuery] string state)
+        catch (Exception e)
         {
-            try
-            {
-                var authorisation = await _shopifyAuthService.HandleCallbackAsync(code, shop, state,
-                    Request.QueryString.ToString());
+            // TODO: Create exception handling system according to custom codes.
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
 
-                var shopInfo = await _shopifyStoreHelper.GetShopifyStoreInfoAsync(shop, authorisation.AccessToken);
-                if (shopInfo == null)
-                    return BadRequest("Invalid Shopify Store ID");
+    [HttpGet("callback")]
+    public async Task<IActionResult> Callback([FromQuery] string code, [FromQuery] string shop, 
+        [FromQuery] string state)
+    {
+        try
+        {
+            var authorisation = await _shopifyAuthService.HandleCallbackAsync(code, shop, state,
+                Request.QueryString.ToString());
+
+            var shopInfo = await _shopifyStoreHelper.GetShopifyStoreInfoAsync(shop, authorisation.AccessToken);
+            if (shopInfo == null)
+                return BadRequest("Invalid Shopify Store ID");
                 
-                return Ok(new {
-                    shop, authorisation.AccessToken, authorisation.GrantedScopes,shopInfo.Name, shopInfo.Email,
-                    shopInfo.Country, shopInfo.Domain, shopInfo.Id, shopInfo.Phone, shopInfo.Description,
-                    JoinedGrantedScopes = String.Join(",", authorisation.GrantedScopes)
-                });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return StatusCode(500, ex.Message);
-            }
+            return Ok(new {
+                shop, authorisation.AccessToken, authorisation.GrantedScopes,shopInfo.Name, shopInfo.Email,
+                shopInfo.Country, shopInfo.Domain, shopInfo.Id, shopInfo.Phone, shopInfo.Description,
+                JoinedGrantedScopes = String.Join(",", authorisation.GrantedScopes)
+            });
         }
-
-        [HttpGet("get-products")]
-        public async Task<IActionResult> GetProductsAsync([FromQuery] string shop, [FromQuery] string accessToken)
+        catch (Exception ex)
         {
-            try
-            {
-                return Ok(await _shopifyDataService.GetProductsAsync(shop, accessToken));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpGet("get-products-count")]
-        public async Task<IActionResult> GetProductsCountAsync([FromQuery] string shop, [FromQuery] string accessToken)
-        {
-            try
-            {
-                return Ok(await _shopifyDataService.GetProductsCount(shop, accessToken));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return BadRequest(e.Message);
-            }
+            Console.WriteLine(ex.Message);
+            return StatusCode(500, ex.Message);
         }
     }
 }
