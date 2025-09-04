b585c6020cecbaa418ee6c07aa2d534e278e3b82
Cleef <cleefgulfan312@gmail.com>
2025-09-04 15:33:16 +0100

    Added and implemented custom data types for product count

---
+++ affiliate-proj/Core/DataTypes/GraphQL/ProductsCountResult.cs
@@ -0,0 +1,11 @@
+﻿namespace affiliate_proj.Core.DataTypes.GraphQL;
+
+public class ProductsCountResult
+{
+    public ProductsCount ProductsCount { get; set; }
+}
+
+public class ProductsCount
+{
+    public int count { get; set; }
+}
