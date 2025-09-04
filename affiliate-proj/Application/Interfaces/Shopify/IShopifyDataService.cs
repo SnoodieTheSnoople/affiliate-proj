02132d7cdbb7f3422c9f1340062a863330547ee3
Cleef <cleefgulfan312@gmail.com>
2025-09-04 17:22:20 +0100

    Removed moved product methods

--- affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
+++ affiliate-proj/Application/Interfaces/Shopify/IShopifyDataService.cs
@@ -10,8 +10,6 @@ namespace affiliate_proj.Application.Interfaces.Shopify;
 
 public interface IShopifyDataService
 {
-    Task<int> GetProductsCount(string shopDomain, string accessToken);
-    Task<GraphResult<CustomListProductsResult>> GetProductsAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Order>> GetOrdersAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Fulfillment>> GetFulfillmentsAsync(string shopDomain, string accessToken, int limit = 250);
     Task<IEnumerable<Refund>> GetRefundsAsync(string shopDomain, string accessToken, int limit = 250);
