dfecfab582c5e5d881b47a3e6211a3f34b408424
Cleef <cleefgulfan312@gmail.com>
2025-09-16 14:17:10 +0100

    Added properties to ShopifyProductMedias to reflect db table

--- affiliate-proj/Core/Entities/ShopifyProductMedias.cs
+++ affiliate-proj/Core/Entities/ShopifyProductMedias.cs
@@ -2,5 +2,12 @@
 
 public class ShopifyProductMedias
 {
-    
+    public Guid MediaId { get; set; }
+    public Guid ProductId { get; set; }
+    public string Alt { get; set; }
+    public string MediaType { get; set; }
+    public string ImageUrl  { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
