3968fee5272182f9bb2198d3904fb472bcff9635
Cleef <cleefgulfan312@gmail.com>
2025-09-16 14:31:01 +0100

    Added SetProductAsync to ShopifyProductRepository

--- affiliate-proj/Application/Interfaces/Shopify/Data/Product/IShopifyProductRepository.cs
+++ affiliate-proj/Application/Interfaces/Shopify/Data/Product/IShopifyProductRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Shopify.Data.Product;
+﻿using affiliate_proj.Core.DTOs.Shopify.Products;
+
+namespace affiliate_proj.Application.Interfaces.Shopify.Data.Product;
 
 public interface IShopifyProductRepository
 {
-    
+    Task<ShopifyProductDTO> SetProductAsync(ShopifyProductDTO shopifyProductDTO);
 }
--- affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
@@ -1,8 +1,12 @@
 ﻿using affiliate_proj.Application.Interfaces.Shopify.Data.Product;
+using affiliate_proj.Core.DTOs.Shopify.Products;
 
 namespace affiliate_proj.Application.Services.Shopify.Data.Product;
 
 public class ShopifyProductRepository : IShopifyProductRepository
 {
-    
+    public async Task<ShopifyProductDTO> SetProductAsync(ShopifyProductDTO shopifyProductDTO)
+    {
+        throw new NotImplementedException();
+    }
 }
