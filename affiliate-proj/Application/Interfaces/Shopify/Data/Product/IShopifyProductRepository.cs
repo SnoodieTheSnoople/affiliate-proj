4b9bd74bdb15873f005a39cde9be90595c7d6b32
Cleef <cleefgulfan312@gmail.com>
2025-09-16 13:33:13 +0100

    Added ShopifyProductRepository and interface

---
+++ affiliate-proj/Application/Interfaces/Shopify/Data/Product/IShopifyProductRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace affiliate_proj.Application.Interfaces.Shopify.Data.Product;
+
+public interface IShopifyProductRepository
+{
+    
+}
---
+++ affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.Shopify.Data.Product;
+
+namespace affiliate_proj.Application.Services.Shopify.Data.Product;
+
+public class ShopifyProductRepository : IShopifyProductRepository
+{
+    
+}
