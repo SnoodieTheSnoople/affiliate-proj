fc4af8091a10150682eb329f32cd022c834b5c4b
Cleef <cleefgulfan312@gmail.com>
2025-09-16 14:14:56 +0100

    Added annotations to properties

--- affiliate-proj/Core/Entities/ShopifyProducts.cs
+++ affiliate-proj/Core/Entities/ShopifyProducts.cs
@@ -1,15 +1,38 @@
-﻿namespace affiliate_proj.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace affiliate_proj.Core.Entities;
 
 public class ShopifyProducts
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid ProductId { get; set; }
+    
+    [Required]
     public Guid StoreId { get; set; }
+    
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
+    
+    [Required]
     public string ShopifyProductId { get; set; }
+    
+    [Required]
     public string Title { get; set; }
+    
+    [Required]
     public string Handle { get; set; }
+    
+    [Required]
     public string HasOnlyDefaultVariant { get; set; }
+    
+    [Required]
     public string OnlineStoreUrl { get; set; }
+    
     public DateTime CreatedAt { get; set; }
+    
     public DateTime UpdatedAt { get; set; }
+    
     public DateTime SyncedAt { get; set; }
 }
