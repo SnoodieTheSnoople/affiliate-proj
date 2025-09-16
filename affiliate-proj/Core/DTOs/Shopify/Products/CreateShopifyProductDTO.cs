3a7b87b4c8f9a4a7ce108e43de842bde5cbb3f92
Cleef <cleefgulfan312@gmail.com>
2025-09-16 14:28:08 +0100

    Added properties to CreateShopifyProductDTO.cs

--- affiliate-proj/Core/DTOs/Shopify/Products/CreateShopifyProductDTO.cs
+++ affiliate-proj/Core/DTOs/Shopify/Products/CreateShopifyProductDTO.cs
@@ -2,5 +2,13 @@
 
 public class CreateShopifyProductDTO
 {
-    
+    public Guid StoreId { get; set; }
+    public string ShopifyProductId { get; set; }
+    public string Title { get; set; }
+    public string Handle { get; set; }
+    public string HasOnlyDefaultVariant { get; set; }
+    public string OnlineStoreUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime SyncedAt { get; set; }
 }
