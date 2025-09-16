08a8cb1b019ef8260f8bfdb55e31d24f6b575db1
Cleef <cleefgulfan312@gmail.com>
2025-09-16 14:18:41 +0100

    Added annotations to ShopifyProductMedias

--- affiliate-proj/Core/Entities/ShopifyProductMedias.cs
+++ affiliate-proj/Core/Entities/ShopifyProductMedias.cs
@@ -1,13 +1,34 @@
-﻿namespace affiliate_proj.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace affiliate_proj.Core.Entities;
 
 public class ShopifyProductMedias
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid MediaId { get; set; }
+    
+    [Required]
     public Guid ProductId { get; set; }
+    
+    [ForeignKey(nameof(ProductId))]
+    public ShopifyProducts Product { get; set; }
+    
+    [Required]
     public string Alt { get; set; }
+    
+    [Required]
     public string MediaType { get; set; }
+    
+    [Required]
     public string ImageUrl  { get; set; }
+    
+    [Required]
     public int Width { get; set; }
+    
+    [Required]
     public int Height { get; set; }
+    
     public DateTime CreatedAt { get; set; }
 }
