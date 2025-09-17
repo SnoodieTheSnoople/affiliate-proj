10adc00b13b6d86036906f208aa4826c7e6d8718
Cleef <cleefgulfan312@gmail.com>
2025-09-17 12:54:44 +0100

    Added ShopifyProductMediaDTO and added properties

---
+++ affiliate-proj/Core/DTOs/Shopify/Products/Media/ShopifyProductMediaDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace affiliate_proj.Core.DTOs.Shopify.Products.Media;
+
+public class ShopifyProductMediaDTO
+{
+    public Guid MediaId { get; set; }
+    public Guid ProductId { get; set; }
+    public string Alt { get; set; }
+    public string MediaType { get; set; }
+    public string ImageUrl  { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
