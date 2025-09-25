431dc6d3d0260d08f79fce52654b5f9bde229722
Cleef <cleefgulfan312@gmail.com>
2025-09-25 17:14:36 +0100

    Added DbContext as dependency

--- affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
@@ -1,10 +1,18 @@
-﻿using affiliate_proj.Application.Interfaces.Shopify.Data.Product;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Application.Interfaces.Shopify.Data.Product;
 using affiliate_proj.Core.DTOs.Shopify.Products;
 
 namespace affiliate_proj.Application.Services.Shopify.Data.Product;
 
 public class ShopifyProductRepository : IShopifyProductRepository
 {
+    private readonly PostgresDbContext _context;
+
+    public ShopifyProductRepository(PostgresDbContext context)
+    {
+        _context = context;
+    }
+
     public async Task<ShopifyProductDTO> SetProductAsync(ShopifyProductDTO shopifyProductDTO)
     {
         throw new NotImplementedException();
