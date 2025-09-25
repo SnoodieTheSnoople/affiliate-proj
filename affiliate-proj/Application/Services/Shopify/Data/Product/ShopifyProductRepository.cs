c0c8790c4939ebdec03933633587d95ea3603a8e
Cleef <cleefgulfan312@gmail.com>
2025-09-25 18:43:39 +0100

    Transformed shopifyProductDTOs to shopifyProductsList for db insert

--- affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Data/Product/ShopifyProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using affiliate_proj.Accessors.DatabaseAccessors;
 using affiliate_proj.Application.Interfaces.Shopify.Data.Product;
 using affiliate_proj.Core.DTOs.Shopify.Products;
+using affiliate_proj.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace affiliate_proj.Application.Services.Shopify.Data.Product;
@@ -22,13 +23,32 @@ public class ShopifyProductRepository : IShopifyProductRepository
     public async Task<List<ShopifyProductDTO>> SetProductsListAsync(List<ShopifyProductDTO> shopifyProductDTOs, 
         Guid storeId)
     {
-        var checkShopifyProductId = shopifyProductDTOs.Select(product => product.ShopifyProductId).ToHashSet();
+        var shopifyProductsList = shopifyProductDTOs.Select(dto => new ShopifyProducts
+        {
+            ProductId = dto.ProductId,
+            StoreId = dto.StoreId,
+            ShopifyProductId = dto.ShopifyProductId,
+            Title = dto.Title,
+            Handle = dto.Handle,
+            HasOnlyDefaultVariant = dto.HasOnlyDefaultVariant,
+            OnlineStoreUrl = dto.OnlineStoreUrl
+        }).ToList();
+        
+        var checkShopifyProductId = shopifyProductsList.Select(product => product.ShopifyProductId).ToHashSet();
         
         var existingProduct = await _context.ShopifyProducts
             .Where(product => product.StoreId == storeId && 
                               checkShopifyProductId.Contains(product.ShopifyProductId))
             .Select(product => product.ShopifyProductId)
             .ToListAsync();
+        
+        // TODO: Consider update if there is an existing entry
+        
+        var newProductList = shopifyProductsList
+            .Where(product => !existingProduct.Contains(product.ShopifyProductId))
+            .ToList();
+        
+        await _context.ShopifyProducts.AddRangeAsync(newProductList);
         
         throw new NotImplementedException();
     }
