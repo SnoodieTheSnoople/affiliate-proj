6f619c2226f75ae5e2ac792c2574765a2e671ff8
cleef <cleefgulfan312@gmail.com>
2025-10-29 16:04:57 +0000

    Added foreign keys to AffiliateLink.cs

--- affiliate-proj/Core/Entities/AffiliateLink.cs
+++ affiliate-proj/Core/Entities/AffiliateLink.cs
@@ -12,8 +12,14 @@ public class AffiliateLink
     [Required]
     public string CreatorId  { get; set; }
     
+    [ForeignKey(nameof(CreatorId))]
+    public Creator Creator { get; set; }
+    
     [Required]
     public string StoreId  { get; set; }
+    
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
     
     [Required]
     public string Link {  get; set; }
