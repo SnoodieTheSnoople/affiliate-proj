839853068c47161225c6a70ef8087018edd5a261
cleef <cleefgulfan312@gmail.com>
2025-10-29 16:06:21 +0000

    Added data annotations and foreign keys to AffiliateCode.cs

--- affiliate-proj/Core/Entities/AffiliateCode.cs
+++ affiliate-proj/Core/Entities/AffiliateCode.cs
@@ -1,13 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace affiliate_proj.Core.Entities;
 
 public class AffiliateCode
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string CodeId { get; set; }
+    
+    [Required]
     public string CreatorId { get; set; }
+    
+    [ForeignKey(nameof(CreatorId))]
+    public Creator Creator { get; set; }
+    
+    [Required]
     public string StoreId { get; set; }
+    
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
+    
+    [Required]
     public string Code { get; set; }
+    
+    [Required]
     public bool IsActive { get; set; }
+    
+    [Required]
     public int ValidFor { get; set; }
+    
+    [Required]
     public DateTime ExpiryDate { get; set; }
     public DateTime CreatedAt { get; set; }
 }
