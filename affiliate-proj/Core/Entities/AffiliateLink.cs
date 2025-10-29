c4496db87b8f71afbdc737816275b607b6c922c5
cleef <cleefgulfan312@gmail.com>
2025-10-29 16:04:26 +0000

    Added data annotations to AffiliateLink.cs

--- affiliate-proj/Core/Entities/AffiliateLink.cs
+++ affiliate-proj/Core/Entities/AffiliateLink.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace affiliate_proj.Core.Entities;
 
 public class AffiliateLink
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string LinkId { get; set; }
+    
+    [Required]
     public string CreatorId  { get; set; }
+    
+    [Required]
     public string StoreId  { get; set; }
+    
+    [Required]
     public string Link {  get; set; }
+    
+    [Required]
     public string RefParam { get; set; }
+    
+    [Required]
     public string ProductLink { get; set; }
     public int Clicks { get; set; }
     public DateTime CreatedAt { get; set; }
