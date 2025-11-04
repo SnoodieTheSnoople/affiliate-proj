49657468cdac69be3b680ca9a5d8c54cafc63317
cleef <cleefgulfan312@gmail.com>
2025-11-04 12:49:29 +0000

    Created and added AffiliateLinkController

---
+++ affiliate-proj/API/Controllers/Account/Affiliate/AffiliateLinkController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Controllers.Account.Affiliate
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AffiliateLinkController : ControllerBase
+    {
+    }
+}
