98f7f2b6568bfb394ce9f582c3992f29722fbc9a
cleef <cleefgulfan312@gmail.com>
2025-11-04 12:58:13 +0000

    Added attributes to AffiliateLinkDTO.cs

--- affiliate-proj/Core/DTOs/Affiliate/Link/AffiliateLinkDTO.cs
+++ affiliate-proj/Core/DTOs/Affiliate/Link/AffiliateLinkDTO.cs
@@ -2,5 +2,12 @@
 
 public class AffiliateLinkDTO
 {
-    
+    public string LinkId { get; set; }
+    public Guid CreatorId  { get; set; }
+    public Guid StoreId  { get; set; }
+    public string Link {  get; set; }
+    public string RefParam { get; set; }
+    public string ProductLink { get; set; }
+    public int Clicks { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
