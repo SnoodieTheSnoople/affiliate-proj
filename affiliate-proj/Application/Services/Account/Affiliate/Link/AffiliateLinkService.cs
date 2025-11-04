be53ee302b6d48dbe1892a46a60ef8cd66cf4c8e
cleef <cleefgulfan312@gmail.com>
2025-11-04 13:29:28 +0000

    Added AccountHelper and StoreService as dependencies

--- affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkService.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkService.cs
@@ -1,10 +1,21 @@
-﻿using affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
+﻿using affiliate_proj.Application.Interfaces.Account;
+using affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
+using affiliate_proj.Application.Interfaces.Store;
 using affiliate_proj.Core.DTOs.Affiliate.Link;
 
 namespace affiliate_proj.Application.Services.Account.Affiliate.Link;
 
 public class AffiliateLinkService : IAffiliateLinkService
 {
+    private readonly IAccountHelper _accountHelper;
+    private readonly IStoreService _storeService;
+
+    public AffiliateLinkService(IAccountHelper accountHelper, IStoreService storeService)
+    {
+        _accountHelper = accountHelper;
+        _storeService = storeService;
+    }
+
     public async Task<AffiliateLinkDTO?> SetAffiliateLinkAsync(CreateAffiliateLinkDTO createAffiliateLinkDto)
     {
         // Validate if Creator and Store exists
