a490bc7c4a342a055437d66f2df83764d338c50e
cleef <cleefgulfan312@gmail.com>
2025-11-04 12:51:57 +0000

    Added basic empty CRUD methods to AffiliateLinkController.cs

--- affiliate-proj/API/Controllers/Account/Affiliate/AffiliateLinkController.cs
+++ affiliate-proj/API/Controllers/Account/Affiliate/AffiliateLinkController.cs
@@ -7,5 +7,60 @@ namespace affiliate_proj.API.Controllers.Account.Affiliate
     [ApiController]
     public class AffiliateLinkController : ControllerBase
     {
+        [HttpPost("set-affiliate-link")]
+        public async Task<IActionResult> SetAffiliateLink()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("get-affiliate-links")]
+        public async Task<IActionResult> GetAffiliateLinks()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpPut("update-affiliate-link")]
+        public async Task<IActionResult> UpdateAffiliateLink()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("delete-affiliate-link")]
+        public async Task<IActionResult> DeleteAffiliateLink()
+        {
+            try 
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
