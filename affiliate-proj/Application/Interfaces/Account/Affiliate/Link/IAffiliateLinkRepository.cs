36c105c9c4269cb2fcef6a07335feaaf217e3143
cleef <cleefgulfan312@gmail.com>
2025-11-04 15:23:22 +0000

    Added SetAffiliateLinkAsync

--- affiliate-proj/Application/Interfaces/Account/Affiliate/Link/IAffiliateLinkRepository.cs
+++ affiliate-proj/Application/Interfaces/Account/Affiliate/Link/IAffiliateLinkRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
+﻿using affiliate_proj.Core.DTOs.Affiliate.Link;
+
+namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
 
 public interface IAffiliateLinkRepository
 {
-    
+    Task<AffiliateLinkDTO> SetAffiliateLinkAsync(CreateAffiliateLinkDTO createAffiliateLinkDto);
 }
