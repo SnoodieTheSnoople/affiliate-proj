8bdedf42fa6192a9a59eb7aeedea8bfbee26fef3
cleef <cleefgulfan312@gmail.com>
2025-11-10 14:38:51 +0000

    Added conversion method from DTO to AffiliateLink

--- affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkRepository.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkRepository.cs
@@ -1,5 +1,6 @@
 ﻿using affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
 using affiliate_proj.Core.DTOs.Affiliate.Link;
+using affiliate_proj.Core.Entities;
 
 namespace affiliate_proj.Application.Services.Account.Affiliate.Link;
 
@@ -8,5 +9,20 @@ public class AffiliateLinkRepository : IAffiliateLinkRepository
     public async Task<AffiliateLinkDTO> SetAffiliateLinkAsync(CreateAffiliateLinkDTO createAffiliateLinkDto)
     {
         throw new NotImplementedException();
+    }
+    
+    private AffiliateLink ConvertFromDTO(AffiliateLink affiliateLink)
+    {
+        return new AffiliateLink
+        {
+            LinkId = affiliateLink.LinkId,
+            CreatorId = affiliateLink.CreatorId,
+            StoreId = affiliateLink.StoreId,
+            Link = affiliateLink.Link,
+            RefParam = affiliateLink.RefParam,
+            ProductLink = affiliateLink.ProductLink,
+            Clicks = affiliateLink.Clicks,
+            CreatedAt = affiliateLink.CreatedAt
+        };
     }
 }
