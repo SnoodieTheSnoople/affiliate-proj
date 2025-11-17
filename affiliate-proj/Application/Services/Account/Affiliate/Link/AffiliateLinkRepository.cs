1a1beb3b2967a77c0255e84a9522fdd70067ef3a
cleef <cleefgulfan312@gmail.com>
2025-11-17 13:26:56 +0000

    Changed method signature to return single value and not list

--- affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkRepository.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateLinkRepository.cs
@@ -85,7 +85,7 @@ public class AffiliateLinkRepository : IAffiliateLinkRepository
         return entities;
     }
 
-    public async Task<List<AffiliateLinkDTO>> GetAffiliateLinkByIdAsync(Guid linkId)
+    public async Task<AffiliateLinkDTO> GetAffiliateLinkByIdAsync(Guid linkId)
     {
         if (linkId == Guid.Empty)
             throw new ArgumentException("Link ID cannot be empty.", nameof(linkId));
