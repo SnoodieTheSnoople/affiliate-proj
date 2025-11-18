17c72071d3ded80a9790efc30327748b79b92ea5
cleef <cleefgulfan312@gmail.com>
2025-11-18 13:26:23 +0000

    Added AffiliateCodeController.cs

---
+++ affiliate-proj/API/Controllers/Account/Affiliate/AffiliateCodeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Controllers.Account.Affiliate;
+
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class AffiliateCodeController : ControllerBase
+{
+    
+}
