cacceedb9853e2944a37d2435d7475cf6eecfb77
cleef <cleefgulfan312@gmail.com>
2025-11-18 15:56:31 +0000

    Added dbContext as dependency

--- affiliate-proj/Application/Services/Account/Affiliate/Code/AffiliateCodeRepository.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Code/AffiliateCodeRepository.cs
@@ -1,8 +1,14 @@
-﻿using affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
 
 namespace affiliate_proj.Application.Services.Account.Affiliate.Code;
 
 public class AffiliateCodeRepository : IAffiliateCodeRepository
 {
-    
+    private readonly PostgresDbContext _dbContext;
+
+    public AffiliateCodeRepository(PostgresDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 }
