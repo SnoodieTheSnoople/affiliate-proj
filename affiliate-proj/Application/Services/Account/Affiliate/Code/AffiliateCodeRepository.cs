31387ed24288e26a4d694d020abe4d14a639b8c9
cleef <cleefgulfan312@gmail.com>
2025-11-18 15:54:38 +0000

    Added AffiliateCodeRepository

---
+++ affiliate-proj/Application/Services/Account/Affiliate/Code/AffiliateCodeRepository.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
+
+namespace affiliate_proj.Application.Services.Account.Affiliate.Code;
+
+public class AffiliateCodeRepository : IAffiliateCodeRepository
+{
+    
+}
