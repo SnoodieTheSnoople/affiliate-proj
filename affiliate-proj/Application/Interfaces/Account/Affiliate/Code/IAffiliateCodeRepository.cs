e39d429d311a51ba92826018da6350e4782f25d5
cleef <cleefgulfan312@gmail.com>
2025-11-18 15:58:38 +0000

    Added GetAffiliateCodeAsync

--- affiliate-proj/Application/Interfaces/Account/Affiliate/Code/IAffiliateCodeRepository.cs
+++ affiliate-proj/Application/Interfaces/Account/Affiliate/Code/IAffiliateCodeRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
+﻿using affiliate_proj.Core.DTOs.Affiliate.Code;
+
+namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
 
 public interface IAffiliateCodeRepository
 {
-    
+    Task<AffiliateCodeDTO?> GetAffiliateCodeAsync(string code);
 }
--- affiliate-proj/Application/Services/Account/Affiliate/Code/AffiliateCodeRepository.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Code/AffiliateCodeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using affiliate_proj.Accessors.DatabaseAccessors;
 using affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
+using affiliate_proj.Core.DTOs.Affiliate.Code;
 
 namespace affiliate_proj.Application.Services.Account.Affiliate.Code;
 
@@ -10,5 +11,10 @@ public class AffiliateCodeRepository : IAffiliateCodeRepository
     public AffiliateCodeRepository(PostgresDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    public async Task<AffiliateCodeDTO?> GetAffiliateCodeAsync(string code)
+    {
+        throw new NotImplementedException();
     }
 }
