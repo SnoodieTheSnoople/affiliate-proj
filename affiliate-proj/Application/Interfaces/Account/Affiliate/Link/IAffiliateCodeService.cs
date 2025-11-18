3744f9da6f5248256573900b440883ef9b003eb7
cleef <cleefgulfan312@gmail.com>
2025-11-18 13:57:25 +0000

    Added SetAffiliateCodeAsync to AffiliateCodeService

--- affiliate-proj/Application/Interfaces/Account/Affiliate/Link/IAffiliateCodeService.cs
+++ affiliate-proj/Application/Interfaces/Account/Affiliate/Link/IAffiliateCodeService.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
+﻿using affiliate_proj.Core.DTOs.Affiliate.Code;
+
+namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
 
 public interface IAffiliateCodeService
 {
-    
+    Task<AffiliateCodeDTO> SetAffiliateCodeAsync(CreateAffiliateCodeDTO createAffiliateCodeDto);
 }
--- affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateCodeService.cs
+++ affiliate-proj/Application/Services/Account/Affiliate/Link/AffiliateCodeService.cs
@@ -1,8 +1,12 @@
 ﻿using affiliate_proj.Application.Interfaces.Account.Affiliate.Link;
+using affiliate_proj.Core.DTOs.Affiliate.Code;
 
 namespace affiliate_proj.Application.Services.Account.Affiliate.Link;
 
 public class AffiliateCodeService : IAffiliateCodeService
 {
-    
+    public async Task<AffiliateCodeDTO> SetAffiliateCodeAsync(CreateAffiliateCodeDTO createAffiliateCodeDto)
+    {
+        throw new NotImplementedException();
+    }
 }
