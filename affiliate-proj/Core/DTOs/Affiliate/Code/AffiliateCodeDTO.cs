160fe74c89fbeda2d70fdfb8a7d8e4b83da2d303
cleef <cleefgulfan312@gmail.com>
2025-11-18 13:39:16 +0000

    Added AffiliateCodeDTO.cs and properties

---
+++ affiliate-proj/Core/DTOs/Affiliate/Code/AffiliateCodeDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace affiliate_proj.Core.DTOs.Affiliate.Code;
+
+public class AffiliateCodeDTO
+{
+    public string CodeId { get; set; }
+    public Guid CreatorId { get; set; }
+    public Guid StoreId { get; set; }
+    public string Code { get; set; }
+    public bool IsActive { get; set; }
+    public int ValidFor { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
