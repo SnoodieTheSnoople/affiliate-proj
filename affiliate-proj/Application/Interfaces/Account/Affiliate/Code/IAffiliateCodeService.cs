5319d1728b252d7ff9ec1ebe1a8b6175e01530f8
cleef <cleefgulfan312@gmail.com>
2025-11-19 14:04:22 +0000

    Added parameters to GetAffiliateCodesBy

--- affiliate-proj/Application/Interfaces/Account/Affiliate/Code/IAffiliateCodeService.cs
+++ affiliate-proj/Application/Interfaces/Account/Affiliate/Code/IAffiliateCodeService.cs
@@ -5,6 +5,6 @@ namespace affiliate_proj.Application.Interfaces.Account.Affiliate.Code;
 public interface IAffiliateCodeService
 {
     Task<AffiliateCodeDTO> SetAffiliateCodeAsync(CreateAffiliateCodeDTO createAffiliateCodeDto);
-    Task<List<AffiliateCodeDTO>> GetAffiliateCodesByCreatorIdAsync();
-    Task<List<AffiliateCodeDTO>> GetAffiliateCodesByStoreIdAsync();
+    Task<List<AffiliateCodeDTO>> GetAffiliateCodesByCreatorIdAsync(Guid creatorId);
+    Task<List<AffiliateCodeDTO>> GetAffiliateCodesByStoreIdAsync(Guid storeId);
 }
