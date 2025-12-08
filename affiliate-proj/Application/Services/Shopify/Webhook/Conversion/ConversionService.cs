1f0963935c868fb6de6989f7798c8fd1fe21055f
cleef <cleefgulfan312@gmail.com>
2025-12-08 16:01:27 +0000

    Added ConversionService.cs

---
+++ affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionService.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.Shopify.Webhook.Conversion;
+
+namespace affiliate_proj.Application.Services.Shopify.Webhook.Conversion;
+
+public class ConversionService : IConversionService
+{
+    
+}
