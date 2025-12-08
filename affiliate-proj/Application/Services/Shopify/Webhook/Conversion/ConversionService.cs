a072b1044c5c39c513a853cdab06d6c59736ba92
cleef <cleefgulfan312@gmail.com>
2025-12-08 16:28:07 +0000

    Added basic entity initialisation

--- affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionService.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionService.cs
@@ -7,6 +7,21 @@ public class ConversionService : IConversionService
     public async Task SetConversionAsync(string shopifyWebhookId, string shopifyOrderId, string code, string landingSite, 
         string referralSite, string currency, string orderStatus, int orderCost)
     {
+        var newConversion = new Core.Entities.Conversion
+        {
+            // Find StoreId
+            Link = String.IsNullOrEmpty(landingSite) ? "" : landingSite,
+            // If link is available then retrieve clicks
+            Code = String.IsNullOrEmpty(code) ? "" : code,
+            ShopifyOrderId = shopifyOrderId,
+            OrderCost = orderCost,
+            Currency = currency,
+            OrderStatus = orderStatus,
+            OrderCreated = DateTime.UtcNow,
+            LandingSite = String.IsNullOrEmpty(landingSite) ? "" : landingSite,
+            LandingSiteRef = String.IsNullOrEmpty(referralSite) ? "" : referralSite,
+            Note = code,
+        };
         throw new NotImplementedException();
     }
 }
