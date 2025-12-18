93992d5194768696d20c09a3f8009698abedb0ca
cleef <cleefgulfan312@gmail.com>
2025-12-18 17:53:19 +0000

    Added parameter using CreateConversion object

--- affiliate-proj/Application/Interfaces/Shopify/Webhook/Conversion/IConversionRepository.cs
+++ affiliate-proj/Application/Interfaces/Shopify/Webhook/Conversion/IConversionRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace affiliate_proj.Application.Interfaces.Shopify.Webhook.Conversion;
+﻿using affiliate_proj.Core.DTOs.Shopify.Conversion;
+
+namespace affiliate_proj.Application.Interfaces.Shopify.Webhook.Conversion;
 
 public interface IConversionRepository
 {
-    Task SetConversionAsync();
+    Task SetConversionAsync(CreateConversion createConversion);
 }
--- affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionRepository.cs
@@ -1,10 +1,11 @@
 ﻿using affiliate_proj.Application.Interfaces.Shopify.Webhook.Conversion;
+using affiliate_proj.Core.DTOs.Shopify.Conversion;
 
 namespace affiliate_proj.Application.Services.Shopify.Webhook.Conversion;
 
 public class ConversionRepository : IConversionRepository
 {
-    public async Task SetConversionAsync()
+    public async Task SetConversionAsync(CreateConversion createConversion)
     {
         throw new NotImplementedException();
     }
