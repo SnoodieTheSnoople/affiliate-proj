584e356b442679cf9dc5761867d43c91cf55a171
cleef <cleefgulfan312@gmail.com>
2025-12-18 14:54:00 +0000

    Added SetConversionAsync in repository

--- affiliate-proj/Application/Interfaces/Shopify/Webhook/Conversion/IConversionRepository.cs
+++ affiliate-proj/Application/Interfaces/Shopify/Webhook/Conversion/IConversionRepository.cs
@@ -2,5 +2,5 @@
 
 public interface IConversionRepository
 {
-    
+    Task SetConversionAsync();
 }
--- affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionRepository.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/Conversion/ConversionRepository.cs
@@ -4,5 +4,8 @@ namespace affiliate_proj.Application.Services.Shopify.Webhook.Conversion;
 
 public class ConversionRepository : IConversionRepository
 {
-    
+    public async Task SetConversionAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
