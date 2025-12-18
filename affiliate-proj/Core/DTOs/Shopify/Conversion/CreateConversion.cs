2666611487842e34d365bc32af40af2ed4bf056e
cleef <cleefgulfan312@gmail.com>
2025-12-18 17:39:43 +0000

    Added properties to CreateConversion.cs

--- affiliate-proj/Core/DTOs/Shopify/Conversion/CreateConversion.cs
+++ affiliate-proj/Core/DTOs/Shopify/Conversion/CreateConversion.cs
@@ -2,5 +2,17 @@
 
 public class CreateConversion
 {
-    
+    public Guid StoreId  { get; set; }
+    public string Link  { get; set; }
+    public int Clicks { get; set; }
+    public string Code  { get; set; }
+    public long ShopifyOrderId { get; set; }
+    public decimal OrderCost  { get; set; }
+    public string Currency { get; set; }
+    public string OrderStatus { get; set; }
+    public DateTime OrderCreated  { get; set; }
+    public string LandingSite  { get; set; }
+    public string LandingSiteRef  { get; set; }
+    public string Note { get; set; }
+    public DateTime CreatedAt { set; get; }
 }
