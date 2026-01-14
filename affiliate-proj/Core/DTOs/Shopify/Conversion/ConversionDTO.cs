d3aac41a2a436b8e654d05cd4a6fba236139a6bf
cleef <cleefgulfan312@gmail.com>
2026-01-14 13:26:47 +0000

    Added all attributes to ConversionDTO

--- affiliate-proj/Core/DTOs/Shopify/Conversion/ConversionDTO.cs
+++ affiliate-proj/Core/DTOs/Shopify/Conversion/ConversionDTO.cs
@@ -2,5 +2,18 @@
 
 public class ConversionDTO
 {
-    
+    public Guid ConversionId { get; set; }
+    public Guid StoreId  { get; set; }
+    public string Link  { get; set; }
+    public int Clicks { get; set; }
+    public string Code  { get; set; }
+    public long ShopifyOrderId { get; set; }
+    public decimal OrderCost  { get; set; }
+    public string Currency { get; set; }
+    public string OrderStatus { get; set; }
+    public DateTime OrderCreated  { get; set; }
+    public string LandingSite  { get; set; }
+    public string LandingSiteRef  { get; set; }
+    public string Note { get; set; }
+    public DateTime CreatedAt { set; get; }
 }
