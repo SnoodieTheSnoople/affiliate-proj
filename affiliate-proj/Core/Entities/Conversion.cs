a98e800f2445dd30b1f16a7b44ebf3f544e843b6
cleef <cleefgulfan312@gmail.com>
2026-01-14 13:43:12 +0000

    Added attributes to Conversion.cs

--- affiliate-proj/Core/Entities/Conversion.cs
+++ affiliate-proj/Core/Entities/Conversion.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace affiliate_proj.Core.Entities;
 
 public class Conversion
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid ConversionId { get; set; }
+    
+    [Required]
     public Guid StoreId  { get; set; }
+    
+    [ForeignKey(nameof(StoreId))]
+    public Store Store { get; set; }
     public string Link  { get; set; }
     public int Clicks { get; set; }
     public string Code  { get; set; }
+    
+    [Required]
     public long ShopifyOrderId { get; set; }
+    
+    [Required]
     public decimal OrderCost  { get; set; }
+    
+    [Required]
     public string Currency { get; set; }
     public string OrderStatus { get; set; }
     public DateTime OrderCreated  { get; set; }
