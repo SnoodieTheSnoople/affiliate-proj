1626d6c6d16a21ce62fc47ce2f0df61a3cb6dcf6
cleef <cleefgulfan312@gmail.com>
2026-01-27 13:53:10 +0000

    Created EarnedCommissionDTO.cs

---
+++ affiliate-proj/Core/DTOs/EarnedCommission/EarnedCommissionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace affiliate_proj.Core.DTOs.EarnedCommission;
+
+public class EarnedCommissionDTO
+{
+    public Guid CommissionId { get; set; }
+    public Guid CreatorId  { get; set; }
+    public Guid StoreId { get; set; }
+    public Guid ConversionId  { get; set; }
+    public decimal OrderCost   { get; set; }
+    public decimal AmtEarned  { get; set; }
+}
