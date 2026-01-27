3aaffe9dc80d2304ec4270eaef95e743732dbf31
cleef <cleefgulfan312@gmail.com>
2026-01-27 14:10:11 +0000

    Added comments

--- affiliate-proj/Application/Services/CommissionAttribution/EarnedCommission.cs
+++ affiliate-proj/Application/Services/CommissionAttribution/EarnedCommission.cs
@@ -2,5 +2,11 @@
 
 public class EarnedCommission
 {
-    
+    /*
+     * 1. Upon order_status fulfilled, calculate the commission based on the predefined rate for the store and creator.
+     * 2. Identify return date.
+     * 3. Past return date, finalize the commission and mark it as earned.
+     * 4. Update the EarnedCommission entity with the finalized amount.
+     * 5. Next step to use TotalCommission
+     */
 }
