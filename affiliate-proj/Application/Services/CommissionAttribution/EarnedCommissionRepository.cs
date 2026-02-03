b53e62e2d0fa5586df4170f7cafe189184204dc8
cleef <cleefgulfan312@gmail.com>
2026-02-03 16:35:22 +0000

    Added helper method to convert DTO to entity

--- affiliate-proj/Application/Services/CommissionAttribution/EarnedCommissionRepository.cs
+++ affiliate-proj/Application/Services/CommissionAttribution/EarnedCommissionRepository.cs
@@ -1,6 +1,7 @@
 ﻿using affiliate_proj.Accessors.DatabaseAccessors;
 using affiliate_proj.Application.Interfaces.CommissionAttribution;
 using affiliate_proj.Core.DTOs.EarnedCommission;
+using affiliate_proj.Core.Entities;
 
 namespace affiliate_proj.Application.Services.CommissionAttribution;
 
@@ -16,5 +17,17 @@ public class EarnedCommissionRepository : IEarnedCommissionRepository
     public async Task<EarnedCommissionDTO> SetEarnedCommission(CreateEarnedCommissionDTO createEarnedCommissionDto)
     {
         throw new NotImplementedException();
+    }
+    
+    private EarnedCommission ConvertDtoToEntity(CreateEarnedCommissionDTO dto)
+    {
+        return new EarnedCommission
+        {
+            CreatorId = dto.CreatorId,
+            StoreId = dto.StoreId,
+            ConversionId = dto.ConversionId,
+            OrderCost = dto.OrderCost,
+            AmtEarned = dto.AmtEarned
+        };
     }
 }
