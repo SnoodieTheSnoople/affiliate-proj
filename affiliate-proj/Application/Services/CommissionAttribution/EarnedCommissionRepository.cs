fa05803f56420eb7830a7797cb2c3c2ea77fc4dd
cleef <cleefgulfan312@gmail.com>
2026-02-03 14:03:13 +0000

    Added EarnedCommissionRepository interface and concrete classes

---
+++ affiliate-proj/Application/Services/CommissionAttribution/EarnedCommissionRepository.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.CommissionAttribution;
+
+namespace affiliate_proj.Application.Services.CommissionAttribution;
+
+public class EarnedCommissionRepository : IEarnedCommissionRepository
+{
+    
+}
