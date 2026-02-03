a523863b608703281b6effc96b3d2de08426fa9a
cleef <cleefgulfan312@gmail.com>
2026-02-03 16:29:40 +0000

    Added DbContext as dependency

--- affiliate-proj/Application/Services/CommissionAttribution/EarnedCommissionRepository.cs
+++ affiliate-proj/Application/Services/CommissionAttribution/EarnedCommissionRepository.cs
@@ -1,10 +1,18 @@
-﻿using affiliate_proj.Application.Interfaces.CommissionAttribution;
+﻿using affiliate_proj.Accessors.DatabaseAccessors;
+using affiliate_proj.Application.Interfaces.CommissionAttribution;
 using affiliate_proj.Core.DTOs.EarnedCommission;
 
 namespace affiliate_proj.Application.Services.CommissionAttribution;
 
 public class EarnedCommissionRepository : IEarnedCommissionRepository
 {
+    private readonly PostgresDbContext _dbContext;
+
+    public EarnedCommissionRepository(PostgresDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public async Task<EarnedCommissionDTO> SetEarnedCommission(CreateEarnedCommissionDTO createEarnedCommissionDto)
     {
         throw new NotImplementedException();
