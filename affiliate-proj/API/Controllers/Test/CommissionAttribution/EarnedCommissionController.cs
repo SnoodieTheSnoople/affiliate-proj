840bc8a7191b05703734af255503c4b6c53bb897
cleef <cleefgulfan312@gmail.com>
2026-02-05 14:32:48 +0000

    Added EarnedCommissionController for testing

---
+++ affiliate-proj/API/Controllers/Test/CommissionAttribution/EarnedCommissionController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace affiliate_proj.API.Controllers.Test.CommissionAttribution
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EarnedCommissionController : ControllerBase
+    {
+    }
+}
