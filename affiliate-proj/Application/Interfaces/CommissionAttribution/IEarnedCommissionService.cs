4f0caf97a27ade5bad827fb0efbbac5b94cbb55c
cleef <cleefgulfan312@gmail.com>
2026-02-05 16:38:16 +0000

    Refactor - Modified CaclulateAttributedCommissionAsync to only handle attribution calculation

--- affiliate-proj/Application/Interfaces/CommissionAttribution/IEarnedCommissionService.cs
+++ affiliate-proj/Application/Interfaces/CommissionAttribution/IEarnedCommissionService.cs
@@ -1,9 +1,10 @@
-﻿using affiliate_proj.Core.DTOs.Shopify.Conversion;
+﻿using affiliate_proj.Core.DTOs.Rates;
+using affiliate_proj.Core.DTOs.Shopify.Conversion;
 
 namespace affiliate_proj.Application.Interfaces.CommissionAttribution;
 
 public interface IEarnedCommissionService
 {
-    public Task CalculateAttributedCommissionAsync(ConversionDTO conversionDto);
+    public Task<decimal> CalculateAttributedCommissionAsync(ConversionDTO conversionDto, CommissionRateDTO commissionRateDto);
     public Task SetEarnedCommissionAsync(ConversionDTO conversionDto);
 }
