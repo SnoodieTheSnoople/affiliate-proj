da2ea9ce20215fb29daf6514fbc212375517e3b4
cleef <cleefgulfan312@gmail.com>
2026-02-05 14:33:40 +0000

    Added dependencies

--- affiliate-proj/API/Controllers/Test/CommissionAttribution/EarnedCommissionController.cs
+++ affiliate-proj/API/Controllers/Test/CommissionAttribution/EarnedCommissionController.cs
@@ -1,3 +1,4 @@
+using affiliate_proj.Application.Interfaces.CommissionAttribution;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +8,13 @@ namespace affiliate_proj.API.Controllers.Test.CommissionAttribution
     [ApiController]
     public class EarnedCommissionController : ControllerBase
     {
+        private readonly IEarnedCommissionService _earnedCommissionService;
+        private readonly ILogger<EarnedCommissionController> _logger;
+
+        public EarnedCommissionController(IEarnedCommissionService earnedCommissionService, ILogger<EarnedCommissionController> logger)
+        {
+            _earnedCommissionService = earnedCommissionService;
+            _logger = logger;
+        }
     }
 }
