b958cca822150cd0606f2b3e5f4577508f480611
cleef <cleefgulfan312@gmail.com>
2026-02-11 16:17:40 +0000

    Added ShopifyOrderWebhookProcessor interface and concrete class

---
+++ affiliate-proj/Application/Interfaces/Shopify/Webhook/Processing/IShopifyOrderWebhookProcessor.cs
@@ -0,0 +1,6 @@
+﻿namespace affiliate_proj.Application.Interfaces.Shopify.Webhook.Processing;
+
+public interface IShopifyOrderWebhookProcessor
+{
+    
+}
---
+++ affiliate-proj/Application/Services/Shopify/Webhook/Processing/ShopifyOrderWebhookProcessor.cs
@@ -0,0 +1,8 @@
+﻿using affiliate_proj.Application.Interfaces.Shopify.Webhook.Processing;
+
+namespace affiliate_proj.Application.Services.Shopify.Webhook.Processing;
+
+public class ShopifyOrderWebhookProcessor : IShopifyOrderWebhookProcessor
+{
+    
+}
