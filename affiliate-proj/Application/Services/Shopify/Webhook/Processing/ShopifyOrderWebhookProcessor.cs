3ec2fbbc9502296710f07efcba51ffe7c9ee2146
cleef <cleefgulfan312@gmail.com>
2026-02-11 16:18:47 +0000

    Added dependencies to ShopifyOrderWebhookProcessor.cs

--- affiliate-proj/Application/Services/Shopify/Webhook/Processing/ShopifyOrderWebhookProcessor.cs
+++ affiliate-proj/Application/Services/Shopify/Webhook/Processing/ShopifyOrderWebhookProcessor.cs
@@ -1,8 +1,19 @@
-﻿using affiliate_proj.Application.Interfaces.Shopify.Webhook.Processing;
+﻿using affiliate_proj.Application.Interfaces.CommissionAttribution;
+using affiliate_proj.Application.Interfaces.Shopify.Webhook.Conversion;
+using affiliate_proj.Application.Interfaces.Shopify.Webhook.Processing;
 
 namespace affiliate_proj.Application.Services.Shopify.Webhook.Processing;
 
 public class ShopifyOrderWebhookProcessor : IShopifyOrderWebhookProcessor
 {
-    
+    private readonly IConversionService _conversionService;
+    private readonly IEarnedCommissionService _earnedCommissionService;
+    private readonly ILogger<ShopifyOrderWebhookProcessor> _logger;
+
+    public ShopifyOrderWebhookProcessor(IConversionService conversionService, IEarnedCommissionService earnedCommissionService, ILogger<ShopifyOrderWebhookProcessor> logger)
+    {
+        _conversionService = conversionService;
+        _earnedCommissionService = earnedCommissionService;
+        _logger = logger;
+    }
 }
