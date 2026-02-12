b26cd232896054f3d06725531d267a65b98aa363
cleef <cleefgulfan312@gmail.com>
2026-02-12 16:19:16 +0000

    Refactor - Changed from booleans to enum

--- affiliate-proj/Core/DataTypes/Records/ConversionStageResult.cs
+++ affiliate-proj/Core/DataTypes/Records/ConversionStageResult.cs
@@ -4,9 +4,14 @@ namespace affiliate_proj.Core.DataTypes.Records;
 
 public sealed record ConversionStageResult
 {
-    public bool IsSuccess { get; init; } 
-    public bool IsIgnored { get; init; }
-    public bool IsDuplicate { get; init; }
+    public ConversionStagingStatus Status { get; init; }
     public Guid ConversionId { get; init; }
     public Conversion? Conversion { get; init; }
+}
+
+public enum ConversionStagingStatus
+{
+    Created,
+    Duplicate,
+    Ignored
 }
