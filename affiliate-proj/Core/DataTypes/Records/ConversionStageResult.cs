107e305afb23c62203cf2c344dc158f36e0b12da
cleef <cleefgulfan312@gmail.com>
2026-02-12 14:42:37 +0000

    Added IsIgnored property to record

--- affiliate-proj/Core/DataTypes/Records/ConversionStageResult.cs
+++ affiliate-proj/Core/DataTypes/Records/ConversionStageResult.cs
@@ -5,6 +5,7 @@ namespace affiliate_proj.Core.DataTypes.Records;
 public sealed record ConversionStageResult
 {
     public bool IsSuccess { get; init; } 
+    public bool IsIgnored { get; init; }
     public bool IsDuplicate { get; init; }
     public Guid ConversionId { get; init; }
 }
